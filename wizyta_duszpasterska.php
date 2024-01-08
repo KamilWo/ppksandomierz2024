27ac8ec6a03f65bf3de0b054cd248c8d357b4206
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2024-01-08 14:38:31 +0000

    2024-01-07

--- wizyta_duszpasterska.php
+++ wizyta_duszpasterska.php
@@ -5,35 +5,56 @@
         <h3>Prządek Wizyty Duszpasterskiej 2023/2024</h3>
     </div>
     <div class="col">
-        <p style="font-weight: bold; text-decoration: underline;">2&nbsp;STYCZNIA (WTOREK)</p>
-        <p>Od&nbsp;godz.&nbsp;10.00 – Sucharzów</p>
-        <p>Od&nbsp;godz.&nbsp;15.00:</p>
-        <p><strong>I&nbsp;KSIĄDZ</strong>: ul.&nbsp;Sadownicza</p>
-        <p><strong>II&nbsp;KSIĄDZ</strong>: ul.&nbsp;Klonowa, od&nbsp;ul.&nbsp;Sucharzowskiej</p>
-        <p><strong>III&nbsp;KSIĄDZ</strong>: ul.&nbsp;Warzywna, Rolnicza, Sucharzowska, Stara Prochownia, Jabłoniowa</p><br>
-
-        <p style="font-weight: bold; text-decoration: underline;">3&nbsp;STYCZNIA (ŚRODA)</p>
-        <p>Od&nbsp;godz.&nbsp;15.00:</p>
-        <p><strong>I&nbsp;KSIĄDZ</strong>: ul.&nbsp;Ożarowska od&nbsp;kościoła w&nbsp;kierunku Ożarowa</p>
-        <p><strong>II&nbsp;KSIĄDZ</strong>: ul.&nbsp;Ożarowska od&nbsp;numeru 123 w&nbsp;kierunku miasta</p>
-        <p><strong>III&nbsp;KSIĄDZ</strong>: ul.&nbsp;Morelowa</p><br>
-
-        <p style="font-weight: bold; text-decoration: underline;">4&nbsp;STYCZNIA (CZWARTEK)</p>
-        <p>Od&nbsp;godz.&nbsp;15.00:</p>
-        <p><strong>I&nbsp;KSIĄDZ</strong>: ul.&nbsp;Wiśniowa</p>
-        <p><strong>II&nbsp;KSIĄDZ</strong>: ul.&nbsp;Asnyka, Frankowskiego, Reymonta</p>
-        <p><strong>III&nbsp;KSIĄDZ</strong>: ul.&nbsp;Obrońców Westerplatte</p><br>
-
-        <p style="font-weight: bold; text-decoration: underline;">5&nbsp;STYCZNIA (PIĄTEK)</p>
-        <p>Od&nbsp;godz.&nbsp;15.00:</p>
-        <p><strong>I&nbsp;KSIĄDZ</strong>: ul.&nbsp;Kochanowskiego</p>
-        <p><strong>II&nbsp;KSIĄDZ</strong>: ul.&nbsp;15&nbsp;Sierpnia</p>
-        <p><strong>III&nbsp;KSIĄDZ</strong>: ul.&nbsp;Wiejska, Wiosenna, Batalionów Chłopskich, Szczęśliwa</p><br>
-
         <p style="font-weight: bold; text-decoration: underline;">8&nbsp;STYCZNIA (PONIEDZIAŁEK)</p>
-        <p>Od&nbsp;godz.&nbsp;15.00:</p>
+        <p><strong>Od&nbsp;godz.&nbsp;15.00:</strong></p>
         <p><strong>2&nbsp;KSIĘŻY</strong>: ul.&nbsp;Harcerska</p>
-        <p><strong>III&nbsp;KSIĄDZ</strong>: ul.&nbsp;Młodożeńca, Przytulna, Różana</p><br>
+        <p><strong>III&nbsp;KSIĄDZ</strong>: ul.&nbsp;Młodożeńca, ul.&nbsp;Przytulna,
+        ul.&nbsp;Różana</p><br>
+
+        <p style="font-weight: bold; text-decoration: underline;">9&nbsp;STYCZNIA (WTOREK)</p>
+        <p><strong>Od&nbsp;godz.&nbsp;15.00:</strong></p>
+        <p><strong>I&nbsp;KSIĄDZ</strong>: ul.&nbsp;Dąbrowskiego</p>
+        <p><strong>II&nbsp;KSIĄDZ</strong>: ul.&nbsp;Skłodowskiej-Curie</p>
+        <p><strong>III&nbsp;KSIĄDZ</strong>: ul.&nbsp;Reja, ul.&nbsp;Modrzewskiego</p><br>
+
+        <p style="font-weight: bold; text-decoration: underline;">10&nbsp;STYCZNIA (ŚRODA)</p>
+        <p><strong>Od&nbsp;godz.&nbsp;15.00:</strong></p>
+        <p><strong>I&nbsp;KSIĄDZ</strong>: ul.&nbsp;Kwiatkowskiego, ul.&nbsp;Orzechowa</p>
+        <p><strong>II&nbsp;KSIĄDZ</strong>: ul.&nbsp;Polna</p>
+        <p><strong>III&nbsp;KSIĄDZ</strong>: ul.&nbsp;Orzeszkowej</p><br>
+
+        <p style="font-weight: bold; text-decoration: underline;">11&nbsp;STYCZNIA (CZWARTEK)</p>
+        <p><strong>Od&nbsp;godz.&nbsp;15.00:</strong></p>
+        <p><strong>I&nbsp;KSIĄDZ</strong>: ul.&nbsp;Maciejowskiego (domy prywatne),
+        ul.&nbsp;Kubeszewskiego, ul.&nbsp;Zielna, ul.&nbsp;Czachowskiego (domy prywatne)</p>
+        <p><strong>II&nbsp;KSIĄDZ</strong>: ul.&nbsp;Wąska</p>
+        <p><strong>III&nbsp;KSIĄDZ</strong>: ul.&nbsp;Grodzisko</p>
+        <p><strong>Od&nbsp;godz.&nbsp;17.00:</strong> ul.&nbsp;Zółkiewskiego 8&nbsp;(blok)
+        - 2&nbsp;księży</p><br>
+
+        <p style="font-weight: bold; text-decoration: underline;">12&nbsp;STYCZNIA (PIĄTEK)</p>
+        <p><strong>Od&nbsp;godz.&nbsp;15.00:</strong></p>
+        <p><strong>2&nbsp;KSIĘŻY</strong>: ul.&nbsp;Mickiewicza nr&nbsp;43, 45, 47</p>
+        <p><strong>III&nbsp;KSIĄDZ</strong>: ul.&nbsp;Mickiewicza nr 57</p><br>
+
+        <p style="font-weight: bold; text-decoration: underline;">13&nbsp;STYCZNIA (SOBOTA)</p>
+        <p><strong>I&nbsp;KSIĄDZ</strong>: <strong>od&nbsp;godz.&nbsp;9.00:</strong>
+        ul.&nbsp;Polskiej Organizacji Wojskowej,<br><strong>od&nbsp;godz.&nbsp;10.30:</strong>
+        ul.&nbsp;Gałczyńskiego,<br><strong>od&nbsp;godz.&nbsp;12.00:</strong>
+        ul.&nbsp;Mickiewicza (domy prywatne);</p>
+        <p><strong>Od&nbsp;godz.&nbsp;9.00:</strong><br>
+        <strong>II&nbsp;KSIĄDZ</strong>:
+        ul.&nbsp;Mickiewicza nr&nbsp;49, 51;<br>
+        <strong>III&nbsp;KSIĄDZ</strong>: ul.&nbsp;Mickiewicza nr&nbsp;53, 55, 59.</p>
+        <p><strong>Od&nbsp;godz.&nbsp;15.00:</strong></p>
+        <p><strong>I&nbsp;KSIĄDZ</strong>: ul.&nbsp;Cieśli nr&nbsp;7</p>
+        <p><strong>II&nbsp;KSIĄDZ</strong>: ul.&nbsp;Cieśli nr&nbsp;9</p><br>
+
+        <p style="font-weight: bold; text-decoration: underline;">15&nbsp;STYCZNIA (PONIEDZIAŁEK)</p>
+        <p><strong>Od&nbsp;godz.&nbsp;15.00:</strong></p>
+        <p><strong>I&nbsp;KSIĄDZ</strong>: ul.&nbsp;Cieśli nr&nbsp;1</p>
+        <p><strong>II&nbsp;KSIĄDZ</strong>: ul.&nbsp;Cieśli nr&nbsp;3</p>
+        <p><strong>III&nbsp;KSIĄDZ</strong>: ul.&nbsp;Czachowskiego nr&nbsp;3</p><br>
     </div>
 </div>
 <br>
