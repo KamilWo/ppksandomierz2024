7cb85c278e06eb5589becafc2bd9bb02b38ce1b7
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2024-01-25 09:17:37 +0000

    2024-01-21

--- archiwum13.php
+++ archiwum13.php
@@ -157,6 +157,39 @@ include 'head.php';
 
                         <div class="card">
                             <div class="card-header">
+                                <h3>14.&nbsp;stycznia 2024&nbsp;r.</h3>
+                                <h3>II&nbsp;NIEDZIELA ZWYKŁA, Rok&nbsp;B</h3>
+                            </div>
+                            <div class="col">
+                                <p>1. Zapraszamy na&nbsp;kolejny wykład z&nbsp;filozofii. Wykład
+                                odbędzie się 16&nbsp;stycznia o&nbsp;godz.&nbsp;16.00 w&nbsp;domu
+                                bł.&nbsp;Ks.&nbsp;Rewery. Temat spotkania: <em>Postmodernizm czyli jak
+                                dzisiaj myśli świat.</em></p>
+                                <p>2. W&nbsp;najbliższy wtorek, zapraszamy na&nbsp;kolejne spotkanie
+                                ABS czyli Akademii Biblijnej w&nbsp;Sandomierzu; Tematem tego
+                                spotkania będzie: Dawid i&nbsp;Salomon – najwięksi królowie
+                                Izraela. Spotkanie będzie we&nbsp;wtorek, 16&nbsp;stycznia
+                                o&nbsp;godz.&nbsp;18.45, w&nbsp;auli Wyższego Seminarium
+                                Duchownego.</p>
+                                <p>3. Pielgrzymka autokarowa do&nbsp;Włoch odbędzie się w&nbsp;dniach:
+                                22 – 29&nbsp;czerwca br. Szczegółowe informacje i&nbsp;zapisy
+                                u&nbsp;ks.&nbsp;Krystiana Musiała.</p>
+                                <p>WIZYTA DUSZPASTERSKA <a href="#pastoral_visit">w&nbsp;zakładce</a>.</p>
+                                <p>Wszystkie rodziny, które nie mogły przyjąć kapłana z&nbsp;wizytą
+                                duszpasterską w&nbsp;wyznaczonym terminie, prosimy zgłaszać
+                                w&nbsp;zakrystii do&nbsp;piątku <strong>19&nbsp;stycznia</strong>.
+                                Wszystkie te&nbsp;rodziny odwiedzimy w&nbsp;sobotę 20&nbsp;stycznia
+                                od&nbsp;godz.&nbsp;9.00.</p>
+                                <p>
+                                    <span style="float: left;">Sandomierz, 14.&nbsp;stycznia 2024&nbsp;r.</span>
+                                    <span style="float: right; text-align: right;">ks.&nbsp;proboszcz Stanisław Chmielewski</span>
+                                </p>
+                            </div>
+                        </div>
+                        <br>
+
+                        <div class="card">
+                            <div class="card-header">
                                 <h3>INTENCJE MSZY ŚWIĘTYCH<br>
                                     8.01. – 14.01.2024&nbsp;r.
                                 </h3>
--- last_post.php
+++ last_post.php
@@ -13,31 +13,40 @@
 
 <div class="card">
     <div class="card-header">
-        <h3>14.&nbsp;stycznia 2024&nbsp;r.</h3>
-        <h3>II&nbsp;NIEDZIELA ZWYKŁA, Rok&nbsp;B</h3>
+        <h3>21.&nbsp;stycznia 2024&nbsp;r.</h3>
+        <h3>III&nbsp;NIEDZIELA ZWYKŁA, Rok&nbsp;B</h3>
     </div>
     <div class="col">
-        <p>1. Zapraszamy na&nbsp;kolejny wykład z&nbsp;filozofii. Wykład
-        odbędzie się 16&nbsp;stycznia o&nbsp;godz.&nbsp;16.00 w&nbsp;domu
-        bł.&nbsp;Ks.&nbsp;Rewery. Temat spotkania: <em>Postmodernizm czyli jak
-        dzisiaj myśli świat.</em></p>
-        <p>2. W&nbsp;najbliższy wtorek, zapraszamy na&nbsp;kolejne spotkanie
-        ABS czyli Akademii Biblijnej w&nbsp;Sandomierzu; Tematem tego
-        spotkania będzie: Dawid i&nbsp;Salomon – najwięksi królowie
-        Izraela. Spotkanie będzie we&nbsp;wtorek, 16&nbsp;stycznia
-        o&nbsp;godz.&nbsp;18.45, w&nbsp;auli Wyższego Seminarium
-        Duchownego.</p>
-        <p>3. Pielgrzymka autokarowa do&nbsp;Włoch odbędzie się w&nbsp;dniach:
-        22 – 29&nbsp;czerwca br. Szczegółowe informacje i&nbsp;zapisy
+        <p>1. Dziękujemy wszystkim tym rodzinom z&nbsp;naszej parafii, które
+        w&nbsp;minionym tygodniu przyjęły nas kapłanów z&nbsp;wizyta
+        duszpasterską. Dziękujemy za&nbsp;wspólną modlitwę i&nbsp;spotkanie.
+        Zakończyliśmy kolędę w&nbsp;naszej parafii. <em>Bóg zapłać</em>
+        wszystkim za&nbsp;wspólny trud w&nbsp;realizacji tego zadania
+        duszpasterskiego.</p>
+        <p>2. W&nbsp;przyszłą niedzielę w&nbsp;naszym parafialnym kościele
+        na&nbsp;Mszy&nbsp;św. o&nbsp;godz.&nbsp;12.30 i&nbsp;chwilę
+        po&nbsp;Niej wystąpi Chór Katedralny. Zapraszamy.</p>
+        <p>3. W&nbsp;przyszłą sobotę – 27&nbsp;stycznia – zapraszamy
+        na&nbsp;kolejny Wieczór Uwielbienia. Początek
+        o&nbsp;godz.&nbsp;18.00.</p>
+        <p>4. Pielgrzymka autokarowa do&nbsp;Włoch odbędzie się w&nbsp;dniach:
+        22 – 29&nbsp;czerwca&nbsp;br. W&nbsp;planie m.in. zwiedzanie
+        Rzymu-Watykanu, Bolonii, Padwy, Loreto, Monte Cassino, Asyżu.
+        Szczegółowe informacje są&nbsp;w&nbsp;gablocie. Zapisy
         u&nbsp;ks.&nbsp;Krystiana Musiała.</p>
-        <p>WIZYTA DUSZPASTERSKA <a href="#pastoral_visit">w&nbsp;zakładce</a>.</p>
-        <p>Wszystkie rodziny, które nie mogły przyjąć kapłana z&nbsp;wizytą
-        duszpasterską w&nbsp;wyznaczonym terminie, prosimy zgłaszać
-        w&nbsp;zakrystii do&nbsp;piątku <strong>19&nbsp;stycznia</strong>.
-        Wszystkie te&nbsp;rodziny odwiedzimy w&nbsp;sobotę 20&nbsp;stycznia
-        od&nbsp;godz.&nbsp;9.00.</p>
+        <p>5. Wdzięczni jesteśmy za&nbsp;modlitwę, dobre słowo, składane
+        ofiary na&nbsp;potrzeby parafii. Wszystkim, którzy w&nbsp;tym
+        tygodniu obchodzą swoje imieniny, urodziny, inne rocznice, składamy
+        najlepsze życzenia z&nbsp;kapłańskim błogosławieństwem. Wszystkim
+        uczestniczącym we&nbsp;Mszy&nbsp;św. życzymy błogosławionej
+        niedzieli.</p>
+        <p>6. Zachęcamy do&nbsp;czytania prasy katolickiej. Są&nbsp;nowe
+        numery „Gościa niedzielnego” i&nbsp;„Niedzieli”.</p>
+        <p>7. Wszystkim babciom i&nbsp;dziadkom z&nbsp;okazji ich święta
+        składamy najserdeczniejsze życzenia, życzymy Bożego błogosławieństwa
+        i&nbsp;wszelkiego dobra.</p>
         <p>
-            <span style="float: left;">Sandomierz, 14.&nbsp;stycznia 2024&nbsp;r.</span>
+            <span style="float: left;">Sandomierz, 21.&nbsp;stycznia 2024&nbsp;r.</span>
             <span style="float: right; text-align: right;">ks.&nbsp;proboszcz Stanisław Chmielewski</span>
         </p>
     </div>
