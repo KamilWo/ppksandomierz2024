1a215e4c8e4f3a15205a6613f57c37924e82f329
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2024-01-28 08:07:07 +0000

    2024-01-28

--- archiwum13.php
+++ archiwum13.php
@@ -19,7 +19,144 @@ include 'head.php';
                     <!-- treść główna -->
                     <div class="col">
                         <h2 class="first-content-header">Archiwum wiadomości 2024 rok</h2>
-                        <h2>Okres 1.&nbsp;stycznia - 21.&nbsp;stycznia 2024&nbsp;r.</h2>
+                        <h2>Okres 1.&nbsp;stycznia - 28.&nbsp;stycznia 2024&nbsp;r.</h2>
+
+                        <div class="card">
+                            <div class="card-header">
+                                <h3>INTENCJE MSZY ŚWIĘTYCH<br>
+                                    22.01. – 28.01.2024&nbsp;r.
+                                </h3>
+                            </div>
+                            <div class="col">
+                        <table class="aktualnosci_table" cellpadding="5" cellspacing="0">
+                            <tbody>
+                                <tr class="actualnosci_day">
+                                    <td><b>PONIEDZIAŁEK, 22&nbsp;stycznia</b></td>
+                                    <td><b></b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>+ Zdzisława Dudka rocz. śm., Teodozję Dudek, zm.
+                                    z&nbsp;rodz. i&nbsp;dusze w&nbsp;czyśćcu cierpiące</td>
+                                </tr>
+                                <tr>
+                                    <td class="center" rowspan="2">18.00</td>
+                                    <td>+ Stanisławę Ręgulską</td>
+                                </tr><tr style="border-bottom: 2px solid black;">
+                                    <td>+ Zygmunta Szpernala</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>WTOREK, 23&nbsp;stycznia</b></td>
+                                    <td><b></b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>+ Krystynę Dubiel</td>
+                                </tr>
+                                <tr>
+                                    <td class="center" rowspan="2">18.00</td>
+                                    <td>+ Walentego, Juliannę Bulirów, Zofię, Wincentego
+                                    Kołodziejów, Marię, Kazimierza Włochów, Irenę, Tadeusza
+                                    Kołaczów, Stanisława, Stanisława Wosiów</td>
+                                </tr><tr style="border-bottom: 2px solid black;">
+                                    <td>+ Zofię Kołodziej</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>ŚRODA, 24&nbsp;stycznia</b></td>
+                                    <td><b>Wspomnienie św.&nbsp;Franciszka Salezego, biskupa
+                                    i&nbsp;Doktora Kościoła</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>+ Henryka Krakowiaka</td>
+                                </tr>
+                                <tr>
+                                    <td class="center" rowspan="2">18.00</td>
+                                    <td>+ Jerzego Sochę</td>
+                                </tr><tr style="border-bottom: 2px solid black;">
+                                    <td>+ Tadeusza Fijałkowskiego</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>CZWARTEK, 25&nbsp;stycznia</b></td>
+                                    <td><b>Święto Nawrócenia Św.&nbsp;Pawła Apostoła</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>+ Grażynę Mądrą</td>
+                                </tr>
+                                <tr>
+                                    <td class="center" rowspan="2">18.00</td>
+                                    <td>+ Józefa Zubę 5&nbsp;rocz. śm.</td>
+                                </tr><tr style="border-bottom: 2px solid black;">
+                                    <td>+ Mariannę i&nbsp;Eugeniusza Jedlińskich</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>PIĄTEK, 26&nbsp;stycznia</b></td>
+                                    <td><b>Wspomnienie Św.&nbsp;biskupów Tymoteusza
+                                    i&nbsp;Tytusa</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>+ Jadwigę i&nbsp;Szczepana Kasprzyckich w&nbsp;rocz.
+                                    śm., zm. z&nbsp;rodz. i&nbsp;dusze w&nbsp;czyśćcu
+                                    cierpiące</td>
+                                </tr>
+                                <tr>
+                                    <td class="center" rowspan="2">18.00</td>
+                                    <td>+ Stanisławę Regulską</td>
+                                </tr><tr style="border-bottom: 2px solid black;">
+                                    <td>+ Macieja Ziemnickiego</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>SOBOTA, 27&nbsp;stycznia</b></td>
+                                    <td><b></b></td>
+                                </tr>
+                                <tr>
+                                    <td class="center" rowspan="2">6.30</td>
+                                    <td>O&nbsp;łaskę dobrej śmierci</td>
+                                </tr><tr style="border-bottom: 2px solid black;">
+                                    <td>+ Reginę Jońca</td>
+                                </tr>
+                                <tr>
+                                    <td class="center" rowspan="2">18.00</td>
+                                    <td>+ Emilię i&nbsp;Edwarda Paluchów</td>
+                                </tr><tr style="border-bottom: 2px solid black;">
+                                    <td>+ Zygmunta Szpernala</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>NIEDZIELA, 28&nbsp;stycznia</b></td>
+                                    <td><b>
+                                        <span style="color: red">IV&nbsp;Niedziela Zwykła</span>
+                                    </b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">7.30</td>
+                                    <td>+ Kazimierza 21&nbsp;rocz. śm., Stefanię, Jana,
+                                    Stanisława, Jerzego Słodowników</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">9.30</td>
+                                    <td>+ Bogusława Sarzyńskiego, Annę, Janusza, Stanisława
+                                    Roźmiejów, Zofię, Mikołaja, Eugeniusza Kapustów</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">11.15</td>
+                                    <td>+ Zofię i&nbsp;Władysława Guściorów</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">12.30</td>
+                                    <td>+ Stanisława i&nbsp;Genowefę, zm. z&nbsp;rodz.</td>
+                                </tr>
+
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>Za&nbsp;parafian</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                            </div>
+                        </div>
+                        <br>
 
                         <div class="card">
                             <div class="card-header">
--- last_intentions.php
+++ last_intentions.php
@@ -1,30 +1,49 @@
 <div class="card">
     <div class="card-header">
         <h3>INTENCJE MSZY ŚWIĘTYCH<br>
-            22.01. – 28.01.2024&nbsp;r.
+            29.01. – 4.02.2024&nbsp;r.
         </h3>
     </div>
     <div class="col">
 <table class="aktualnosci_table" cellpadding="5" cellspacing="0">
     <tbody>
         <tr class="actualnosci_day">
-            <td><b>PONIEDZIAŁEK, 22&nbsp;stycznia</b></td>
+            <td><b>PONIEDZIAŁEK, 29&nbsp;stycznia</b></td>
             <td><b></b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>+ Zdzisława Dudka rocz. śm., Teodozję Dudek, zm.
-            z&nbsp;rodz. i&nbsp;dusze w&nbsp;czyśćcu cierpiące</td>
+            <td>+ Zofię Gołębiowską</td>
         </tr>
         <tr>
             <td class="center" rowspan="2">18.00</td>
-            <td>+ Stanisławę Ręgulską</td>
+            <td>Dziękczynna z&nbsp;racji urodzin Krystiana</td>
         </tr><tr style="border-bottom: 2px solid black;">
-            <td>+ Zygmunta Szpernala</td>
+            <td>Dziękczynno-błagalna z&nbsp;racji urodzin Agnieszki
+            o&nbsp;Boże Błog., potrzebne łaski i&nbsp;opiekę Matki
+            Bożej</td>
         </tr>
         <tr class="actualnosci_day">
-            <td><b>WTOREK, 23&nbsp;stycznia</b></td>
+            <td><b>WTOREK, 30&nbsp;stycznia</b></td>
             <td><b></b></td>
+        </tr>
+        <tr>
+            <td class="center" rowspan="2">6.30</td>
+            <td>+ Krystynę Dubiel</td>
+        </tr><tr style="border-bottom: 2px solid black;">
+            <td>+ Czesława Dudę</td>
+        </tr>
+        <tr>
+            <td class="center" rowspan="2">18.00</td>
+            <td>+ Tadeusza Fijałkowskiego</td>
+        </tr><tr style="border-bottom: 2px solid black;">
+            <td>Dziękczynno-błagalna i&nbsp;Miłosierdzie Boże, Wiarę,
+            Nadzieję i&nbsp;Miłość, łaskę pokory, nawrócenie
+            i&nbsp;uzdrowienie w&nbsp;rodzinie</td>
+        </tr>
+        <tr class="actualnosci_day">
+            <td><b>ŚRODA, 31&nbsp;stycznia</b></td>
+            <td><b>Wspomnienie św.&nbsp;Jana Bosko, prezbitera</b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
@@ -32,102 +51,96 @@
         </tr>
         <tr>
             <td class="center" rowspan="2">18.00</td>
-            <td>+ Walentego, Juliannę Bulirów, Zofię, Wincentego
-            Kołodziejów, Marię, Kazimierza Włochów, Irenę, Tadeusza
-            Kołaczów, Stanisława, Stanisława Wosiów</td>
+            <td>+ Marię Bryła</td>
         </tr><tr style="border-bottom: 2px solid black;">
-            <td>+ Zofię Kołodziej</td>
+            <td>+ Kazimierza i&nbsp;Mariannę Kieszkowskich, zm.
+            z&nbsp;rodz. Kieszkowskich i&nbsp;Nizielskich</td>
         </tr>
         <tr class="actualnosci_day">
-            <td><b>ŚRODA, 24&nbsp;stycznia</b></td>
-            <td><b>Wspomnienie św.&nbsp;Franciszka Salezego, biskupa
-            i&nbsp;Doktora Kościoła</b></td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">6.30</td>
-            <td>+ Henryka Krakowiaka</td>
-        </tr>
-        <tr>
-            <td class="center" rowspan="2">18.00</td>
-            <td>+ Jerzego Sochę</td>
-        </tr><tr style="border-bottom: 2px solid black;">
-            <td>+ Tadeusza Fijałkowskiego</td>
-        </tr>
-        <tr class="actualnosci_day">
-            <td><b>CZWARTEK, 25&nbsp;stycznia</b></td>
-            <td><b>Święto Nawrócenia Św.&nbsp;Pawła Apostoła</b></td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">6.30</td>
-            <td>+ Grażynę Mądrą</td>
-        </tr>
-        <tr>
-            <td class="center" rowspan="2">18.00</td>
-            <td>+ Józefa Zubę 5&nbsp;rocz. śm.</td>
-        </tr><tr style="border-bottom: 2px solid black;">
-            <td>+ Mariannę i&nbsp;Eugeniusza Jedlińskich</td>
-        </tr>
-        <tr class="actualnosci_day">
-            <td><b>PIĄTEK, 26&nbsp;stycznia</b></td>
-            <td><b>Wspomnienie Św.&nbsp;biskupów Tymoteusza
-            i&nbsp;Tytusa</b></td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">6.30</td>
-            <td>+ Jadwigę i&nbsp;Szczepana Kasprzyckich w&nbsp;rocz.
-            śm., zm. z&nbsp;rodz. i&nbsp;dusze w&nbsp;czyśćcu
-            cierpiące</td>
-        </tr>
-        <tr>
-            <td class="center" rowspan="2">18.00</td>
-            <td>+ Stanisławę Regulską</td>
-        </tr><tr style="border-bottom: 2px solid black;">
-            <td>+ Macieja Ziemnickiego</td>
-        </tr>
-        <tr class="actualnosci_day">
-            <td><b>SOBOTA, 27&nbsp;stycznia</b></td>
-            <td><b></b></td>
+            <td><b>CZWARTEK, 1&nbsp;lutego</b></td>
+            <td><b>I&nbsp;Czwartek miesiąca</b></td>
         </tr>
         <tr>
             <td class="center" rowspan="2">6.30</td>
-            <td>O&nbsp;łaskę dobrej śmierci</td>
+            <td>O&nbsp;świętość kapłanów i&nbsp;nowe powołania</td>
         </tr><tr style="border-bottom: 2px solid black;">
-            <td>+ Reginę Jońca</td>
+            <td>+ Franciszka Turka</td>
         </tr>
         <tr>
             <td class="center" rowspan="2">18.00</td>
-            <td>+ Emilię i&nbsp;Edwarda Paluchów</td>
+            <td>+ Stanisławę, Jana, Stanisława, Witolda, Mariana,
+            Krystynę, Mariana i&nbsp;Janinę</td>
         </tr><tr style="border-bottom: 2px solid black;">
-            <td>+ Zygmunta Szpernala</td>
+            <td>+ Marię Bryła</td>
         </tr>
         <tr class="actualnosci_day">
-            <td><b>NIEDZIELA, 28&nbsp;stycznia</b></td>
+            <td><b>PIĄTEK, 2&nbsp;lutego</b></td>
+            <td><b><span style="color: red">Święto Ofiarowania
+            Pańskiego</span><br>
+            I&nbsp;Piątek miesiąca</b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>O&nbsp;łaskę żywej wiary dla dzieci
+            i&nbsp;chrześniaków</td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">9.00</td>
+            <td>+ Franciszka 7&nbsp;rocz. śm., Barbarę Misztalów</td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">14.00</td>
+            <td>MSZA ŚWIĘTA ZE&nbsp;ZGROMADZENIAMI ZAKONNYMI</td>
+        </tr>
+        <tr>
+            <td class="center" rowspan="2">18.00</td>
+            <td>+ Józefa Wasińskiego – miesiąc po&nbsp;śmierci</td>
+        </tr><tr style="border-bottom: 2px solid black;">
+            <td>+ Jerzego Sochę</td>
+        </tr>
+        <tr class="actualnosci_day">
+            <td><b>SOBOTA, 3&nbsp;lutego</b></td>
+            <td><b>I&nbsp;Sobota miesiąca</b></td>
+        </tr>
+        <tr>
+            <td class="center" rowspan="2">6.30</td>
+            <td>+ Krzysztofa Czerwca</td>
+        </tr><tr style="border-bottom: 2px solid black;">
+            <td>+ Leszka Bobolę – miesiąc po&nbsp;śmierci</td>
+        </tr>
+        <tr>
+            <td class="center" rowspan="2">18.00</td>
+            <td>LM</td>
+        </tr><tr style="border-bottom: 2px solid black;">
+            <td>+ Marię Bryła</td>
+        </tr>
+        <tr class="actualnosci_day">
+            <td><b>NIEDZIELA, 4&nbsp;lutego</b></td>
             <td><b>
-                <span style="color: red">IV&nbsp;Niedziela Zwykła</span>
+                <span style="color: red">V&nbsp;Niedziela Zwykła</span>
             </b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">7.30</td>
-            <td>+ Kazimierza 21&nbsp;rocz. śm., Stefanię, Jana,
-            Stanisława, Jerzego Słodowników</td>
+            <td>Za&nbsp;parafian</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">9.30</td>
-            <td>+ Bogusława Sarzyńskiego, Annę, Janusza, Stanisława
-            Roźmiejów, Zofię, Mikołaja, Eugeniusza Kapustów</td>
+            <td>+ Janinę i&nbsp;Wacława Orłowskich, Stefana
+            Szpakowskiego i&nbsp;Mariannę Dyl</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">11.15</td>
-            <td>+ Zofię i&nbsp;Władysława Guściorów</td>
+            <td>KŻR – od&nbsp;Róży Róż. – zelator Ewa Łukawska</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">12.30</td>
-            <td>+ Stanisława i&nbsp;Genowefę, zm. z&nbsp;rodz.</td>
+            <td>+ Kazimierza Kondeusza 9&nbsp;rocz. śm.</td>
         </tr>
 
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>Za&nbsp;parafian</td>
+            <td>+ Jana i&nbsp;Andrzeja Popków</td>
         </tr>
     </tbody>
 </table>
