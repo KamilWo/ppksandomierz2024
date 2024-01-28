0c02fe4269cc9df45f92ca57d187ca72d5dbd280
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2024-01-28 08:17:02 +0000

    2024-01-28

--- archiwum13.php
+++ archiwum13.php
@@ -160,6 +160,48 @@ include 'head.php';
 
                         <div class="card">
                             <div class="card-header">
+                                <h3>21.&nbsp;stycznia 2024&nbsp;r.</h3>
+                                <h3>III&nbsp;NIEDZIELA ZWYKŁA, Rok&nbsp;B</h3>
+                            </div>
+                            <div class="col">
+                                <p>1. Dziękujemy wszystkim tym rodzinom z&nbsp;naszej parafii, które
+                                w&nbsp;minionym tygodniu przyjęły nas kapłanów z&nbsp;wizyta
+                                duszpasterską. Dziękujemy za&nbsp;wspólną modlitwę i&nbsp;spotkanie.
+                                Zakończyliśmy kolędę w&nbsp;naszej parafii. <em>Bóg zapłać</em>
+                                wszystkim za&nbsp;wspólny trud w&nbsp;realizacji tego zadania
+                                duszpasterskiego.</p>
+                                <p>2. W&nbsp;przyszłą niedzielę w&nbsp;naszym parafialnym kościele
+                                na&nbsp;Mszy&nbsp;św. o&nbsp;godz.&nbsp;12.30 i&nbsp;chwilę
+                                po&nbsp;Niej wystąpi Chór Katedralny. Zapraszamy.</p>
+                                <p>3. W&nbsp;przyszłą sobotę – 27&nbsp;stycznia – zapraszamy
+                                na&nbsp;kolejny Wieczór Uwielbienia. Początek
+                                o&nbsp;godz.&nbsp;18.00.</p>
+                                <p>4. Pielgrzymka autokarowa do&nbsp;Włoch odbędzie się w&nbsp;dniach:
+                                22 – 29&nbsp;czerwca&nbsp;br. W&nbsp;planie m.in. zwiedzanie
+                                Rzymu-Watykanu, Bolonii, Padwy, Loreto, Monte Cassino, Asyżu.
+                                Szczegółowe informacje są&nbsp;w&nbsp;gablocie. Zapisy
+                                u&nbsp;ks.&nbsp;Krystiana Musiała.</p>
+                                <p>5. Wdzięczni jesteśmy za&nbsp;modlitwę, dobre słowo, składane
+                                ofiary na&nbsp;potrzeby parafii. Wszystkim, którzy w&nbsp;tym
+                                tygodniu obchodzą swoje imieniny, urodziny, inne rocznice, składamy
+                                najlepsze życzenia z&nbsp;kapłańskim błogosławieństwem. Wszystkim
+                                uczestniczącym we&nbsp;Mszy&nbsp;św. życzymy błogosławionej
+                                niedzieli.</p>
+                                <p>6. Zachęcamy do&nbsp;czytania prasy katolickiej. Są&nbsp;nowe
+                                numery „Gościa niedzielnego” i&nbsp;„Niedzieli”.</p>
+                                <p>7. Wszystkim babciom i&nbsp;dziadkom z&nbsp;okazji ich święta
+                                składamy najserdeczniejsze życzenia, życzymy Bożego błogosławieństwa
+                                i&nbsp;wszelkiego dobra.</p>
+                                <p>
+                                    <span style="float: left;">Sandomierz, 21.&nbsp;stycznia 2024&nbsp;r.</span>
+                                    <span style="float: right; text-align: right;">ks.&nbsp;proboszcz Stanisław Chmielewski</span>
+                                </p>
+                            </div>
+                        </div>
+                        <br>
+
+                        <div class="card">
+                            <div class="card-header">
                                 <h3>INTENCJE MSZY ŚWIĘTYCH<br>
                                     15.01. – 21.01.2024&nbsp;r.
                                 </h3>
