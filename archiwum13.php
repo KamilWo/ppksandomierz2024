0775e5370f5df41dd4e51beb1910044e9c3209a0
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2024-02-26 18:59:15 +0000

    2024-02-25

--- archiwum13.php
+++ archiwum13.php
@@ -448,6 +448,63 @@ include 'head.php';
 
                         <div class="card">
                             <div class="card-header">
+                                <h3>28.&nbsp;stycznia 2024&nbsp;r.</h3>
+                                <h3>IV&nbsp;NIEDZIELA ZWYKŁA, Rok&nbsp;B</h3>
+                            </div>
+                            <div class="col">
+                                <p>1. <strong>W&nbsp;najbliższy piątek 2&nbsp;lutego - Święto
+                                Ofiarowania Pańskiego – w&nbsp;tradycji – Matki Boskiej
+                                Gromnicznej.</strong> Poświęcenie gromnic w&nbsp;czasie Mszy&nbsp;św.
+                                o&nbsp;godz.&nbsp;9.00, 14.00 i&nbsp;o&nbsp;18.00. To&nbsp;także
+                                Dzień Życia Konsekrowanego – stąd w&nbsp;naszym kościele
+                                o&nbsp;godz.&nbsp;14.00. Msza&nbsp;św., którą będzie
+                                ks.&nbsp;Bp&nbsp;Ordynariusz wraz kapłanami zakonnymi
+                                i&nbsp;diecezjalnymi, brały będą udział w&nbsp;modlitwie Siostry
+                                Zakonne z&nbsp;różnych zgromadzeń. W&nbsp;tej modlitwie polecać Panu
+                                Bogu będziemy osoby konsekrowane i&nbsp;prosić o&nbsp;nowe powołania.
+                                Zapraszamy parafian do&nbsp;uczestnictwa w&nbsp;tej Mszy&nbsp;św.
+                                o&nbsp;godz.&nbsp;14.00. Taca zebrana w&nbsp;tym dniu przeznaczona
+                                jest dla Wspólnoty Sióstr Klarysek w&nbsp;Sandomierzu.</p>
+                                <p>2. W&nbsp;najbliższą środę, tj.&nbsp;31&nbsp;stycznia br.
+                                w&nbsp;naszym kościele parafialnym o&nbsp;godz.&nbsp;10.00, odbędzie
+                                się przegląd kolęd i&nbsp;pastorałek organizowany przez Przedszkole
+                                nr&nbsp;3 i&nbsp;Szkołę Podstawową nr&nbsp;2 w&nbsp;Sandomierzu.
+                                W&nbsp;przeglądzie wezmą udział dzieci ze&nbsp;wszystkich
+                                sandomierskich przeszkoli. Zapraszamy.</p>
+                                <p>3. W&nbsp;tym tygodniu Pierwszy czwartek, piątek i&nbsp;Pierwsza
+                                sobota miesiąca. W&nbsp;Pierwszy Piątek Miesiąca, spowiedź w&nbsp;tym
+                                dniu od&nbsp;godz.&nbsp;17.00.</p>
+                                <p>4. W&nbsp;najbliższą sobotę o&nbsp;godz.&nbsp;17.15. – nabożeństwo
+                                Różańcowe poprowadzi Legion Maryi z&nbsp;naszej parafii.</p>
+                                <p>5. W&nbsp;przyszłą niedzielę w&nbsp;kościele, pół godziny przed
+                                Mszą&nbsp;św. o&nbsp;godz.&nbsp;11.15 – adoracja Najśw. Sakramentu
+                                połączona z&nbsp;modlitwą różańcową, którą poprowadzi Koło Żywego
+                                Różańca z&nbsp;naszej parafii i&nbsp;zmiana tajemnic różańcowych.
+                                Zapraszamy do&nbsp;udziału w&nbsp;tej modlitwie.</p>
+                                <p>6. Są&nbsp;jeszcze wolne miejsca na&nbsp;pielgrzymkę autokarową
+                                do&nbsp;Włoch, która odbędzie się w&nbsp;dniach: 22 – 29&nbsp;czerwca
+                                br. Plan pielgrzymki – w&nbsp;gablocie i&nbsp;na&nbsp;stronie
+                                internetowej parafii. Szczegółowe informacje i&nbsp;zapisy
+                                u&nbsp;ks.&nbsp;Krystiana Musiała.</p>
+                                <p>7. Spotkanie wspólnoty „Różaniec rodziców za&nbsp;dzieci”
+                                w&nbsp;środę – 31&nbsp;stycznia. O&nbsp;godz.&nbsp;18.00 – Msza Święta
+                                i&nbsp;spotkanie w&nbsp;Sali na&nbsp;plebanii.</p>
+                                <p>8. Zachęcamy do&nbsp;czytania prasy katolickiej: „Niedziela”, „Gość
+                                Niedzielny”.</p>
+                                <p>9. Wszystkim, którzy w&nbsp;tym tygodniu obchodzą swoje imieniny,
+                                urodziny, inne rocznice, składamy najlepsze życzenia z&nbsp;kapłańskim
+                                błogosławieństwem. Wszystkim uczestniczącym we&nbsp;Mszy&nbsp;św.
+                                życzymy błogosławionej niedzieli.</p>
+                                <p>
+                                    <span style="float: left;">Sandomierz, 28.&nbsp;stycznia 2024&nbsp;r.</span>
+                                    <span style="float: right; text-align: right;">ks.&nbsp;proboszcz Stanisław Chmielewski</span>
+                                </p>
+                            </div>
+                        </div>
+                        <br>
+
+                        <div class="card">
+                            <div class="card-header">
                                 <h3>INTENCJE MSZY ŚWIĘTYCH<br>
                                     22.01. – 28.01.2024&nbsp;r.
                                 </h3>
--- last_post.php
+++ last_post.php
@@ -13,55 +13,45 @@
 
 <div class="card">
     <div class="card-header">
-        <h3>28.&nbsp;stycznia 2024&nbsp;r.</h3>
-        <h3>IV&nbsp;NIEDZIELA ZWYKŁA, Rok&nbsp;B</h3>
+        <h3>25.&nbsp;lutego 2024&nbsp;r.</h3>
+        <h3>II&nbsp;NIEDZIELA WIELKIEGO POSTU, Rok&nbsp;B</h3>
     </div>
     <div class="col">
-        <p>1. <strong>W&nbsp;najbliższy piątek 2&nbsp;lutego - Święto
-        Ofiarowania Pańskiego – w&nbsp;tradycji – Matki Boskiej
-        Gromnicznej.</strong> Poświęcenie gromnic w&nbsp;czasie Mszy&nbsp;św.
-        o&nbsp;godz.&nbsp;9.00, 14.00 i&nbsp;o&nbsp;18.00. To&nbsp;także
-        Dzień Życia Konsekrowanego – stąd w&nbsp;naszym kościele
-        o&nbsp;godz.&nbsp;14.00. Msza&nbsp;św., którą będzie
-        ks.&nbsp;Bp&nbsp;Ordynariusz wraz kapłanami zakonnymi
-        i&nbsp;diecezjalnymi, brały będą udział w&nbsp;modlitwie Siostry
-        Zakonne z&nbsp;różnych zgromadzeń. W&nbsp;tej modlitwie polecać Panu
-        Bogu będziemy osoby konsekrowane i&nbsp;prosić o&nbsp;nowe powołania.
-        Zapraszamy parafian do&nbsp;uczestnictwa w&nbsp;tej Mszy&nbsp;św.
-        o&nbsp;godz.&nbsp;14.00. Taca zebrana w&nbsp;tym dniu przeznaczona
-        jest dla Wspólnoty Sióstr Klarysek w&nbsp;Sandomierzu.</p>
-        <p>2. W&nbsp;najbliższą środę, tj.&nbsp;31&nbsp;stycznia br.
-        w&nbsp;naszym kościele parafialnym o&nbsp;godz.&nbsp;10.00, odbędzie
-        się przegląd kolęd i&nbsp;pastorałek organizowany przez Przedszkole
-        nr&nbsp;3 i&nbsp;Szkołę Podstawową nr&nbsp;2 w&nbsp;Sandomierzu.
-        W&nbsp;przeglądzie wezmą udział dzieci ze&nbsp;wszystkich
-        sandomierskich przeszkoli. Zapraszamy.</p>
-        <p>3. W&nbsp;tym tygodniu Pierwszy czwartek, piątek i&nbsp;Pierwsza
-        sobota miesiąca. W&nbsp;Pierwszy Piątek Miesiąca, spowiedź w&nbsp;tym
-        dniu od&nbsp;godz.&nbsp;17.00.</p>
-        <p>4. W&nbsp;najbliższą sobotę o&nbsp;godz.&nbsp;17.15. – nabożeństwo
+        <p>1. Z&nbsp;racji, że&nbsp;w&nbsp;naszym kościele jest Stacyjna Droga
+        Krzyżowa, wyjątkowo, w&nbsp;najbliższy piątek Droga Krzyżowa
+        o&nbsp;godz.&nbsp;17.00 - zapraszamy do&nbsp;udziału.</p>
+        <p>2. <strong>Parafialne Rekolekcje Wielkopostne</strong>
+        w&nbsp;naszej parafii rozpoczną się w&nbsp;<strong>piątą niedzielę
+        Wielkiego Postu</strong>, tj.&nbsp;17&nbsp;marca br. Parafialna
+        Spowiedź Wielkopostna w&nbsp;Uroczystość św.&nbsp;Józefa,
+        tj.&nbsp;19&nbsp;marca br.</p>
+        <p>3. W&nbsp;każdą niedzielę Wielkiego Postu nabożeństwo „Gorzkich
+        Żali” z&nbsp;nauką pasyjną o&nbsp;godz.&nbsp;17.15, następnie Msza
+        Święta.</p>
+        <p>4. W&nbsp;ramach jałmużny wielkopostnej, dziś po&nbsp;Mszach św.
+        zbiórka do&nbsp;puszek na&nbsp;rzecz dzieła pomocy misjonarzom
+        „Ad&nbsp;Gentes”.</p>
+        <p>5. W&nbsp;tym tygodniu Pierwszy piątek i&nbsp;Pierwsza sobota
+        miesiąca. W&nbsp;Pierwszy Piątek Miesiąca, spowiedź w&nbsp;tym dniu
+        od&nbsp;godz.&nbsp;17.00.</p>
+        <p>6. W&nbsp;najbliższą sobotę o&nbsp;godz.&nbsp;17.15. – nabożeństwo
         Różańcowe poprowadzi Legion Maryi z&nbsp;naszej parafii.</p>
-        <p>5. W&nbsp;przyszłą niedzielę w&nbsp;kościele, pół godziny przed
+        <p>7. W&nbsp;przyszłą niedzielę w&nbsp;kościele, pół godziny przed
         Mszą&nbsp;św. o&nbsp;godz.&nbsp;11.15 – adoracja Najśw. Sakramentu
         połączona z&nbsp;modlitwą różańcową, którą poprowadzi Koło Żywego
         Różańca z&nbsp;naszej parafii i&nbsp;zmiana tajemnic różańcowych.
         Zapraszamy do&nbsp;udziału w&nbsp;tej modlitwie.</p>
-        <p>6. Są&nbsp;jeszcze wolne miejsca na&nbsp;pielgrzymkę autokarową
-        do&nbsp;Włoch, która odbędzie się w&nbsp;dniach: 22 – 29&nbsp;czerwca
-        br. Plan pielgrzymki – w&nbsp;gablocie i&nbsp;na&nbsp;stronie
-        internetowej parafii. Szczegółowe informacje i&nbsp;zapisy
+        <p>8. Są&nbsp;jeszcze wolne miejsce w&nbsp;drugim autokarze
+        na&nbsp;pielgrzymkę do&nbsp;Włoch. Szczegółowe informacje
         u&nbsp;ks.&nbsp;Krystiana Musiała.</p>
-        <p>7. Spotkanie wspólnoty „Różaniec rodziców za&nbsp;dzieci”
-        w&nbsp;środę – 31&nbsp;stycznia. O&nbsp;godz.&nbsp;18.00 – Msza Święta
-        i&nbsp;spotkanie w&nbsp;Sali na&nbsp;plebanii.</p>
-        <p>8. Zachęcamy do&nbsp;czytania prasy katolickiej: „Niedziela”, „Gość
+        <p>9. Zachęcamy do&nbsp;czytania prasy katolickiej: „Niedziela”, „Gość
         Niedzielny”.</p>
-        <p>9. Wszystkim, którzy w&nbsp;tym tygodniu obchodzą swoje imieniny,
-        urodziny, inne rocznice, składamy najlepsze życzenia z&nbsp;kapłańskim
-        błogosławieństwem. Wszystkim uczestniczącym we&nbsp;Mszy&nbsp;św.
-        życzymy błogosławionej niedzieli.</p>
+        <p>10. W&nbsp;zakrystii są&nbsp;do&nbsp;nabycia Baranki wielkanocne
+        w&nbsp;cenie 8&nbsp;zł.</p>
+        <p>11. Wszystkim uczestniczącym we&nbsp;Mszy&nbsp;św. życzymy
+        błogosławionej niedzieli.</p>
         <p>
-            <span style="float: left;">Sandomierz, 28.&nbsp;stycznia 2024&nbsp;r.</span>
+            <span style="float: left;">Sandomierz, 25.&nbsp;lutego 2024&nbsp;r.</span>
             <span style="float: right; text-align: right;">ks.&nbsp;proboszcz Stanisław Chmielewski</span>
         </p>
     </div>
