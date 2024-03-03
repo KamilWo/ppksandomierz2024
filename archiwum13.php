29bcc1396f8120cf6b353e6d3a861f1131df4607
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2024-03-03 22:32:59 +0000

    2024-03-03

--- archiwum13.php
+++ archiwum13.php
@@ -23,6 +23,53 @@ include 'head.php';
 
                         <div class="card">
                             <div class="card-header">
+                                <h3>25.&nbsp;lutego 2024&nbsp;r.</h3>
+                                <h3>II&nbsp;NIEDZIELA WIELKIEGO POSTU, Rok&nbsp;B</h3>
+                            </div>
+                            <div class="col">
+                                <p>1. Z&nbsp;racji, że&nbsp;w&nbsp;naszym kościele jest Stacyjna Droga
+                                Krzyżowa, wyjątkowo, w&nbsp;najbliższy piątek Droga Krzyżowa
+                                o&nbsp;godz.&nbsp;17.00 - zapraszamy do&nbsp;udziału.</p>
+                                <p>2. <strong>Parafialne Rekolekcje Wielkopostne</strong>
+                                w&nbsp;naszej parafii rozpoczną się w&nbsp;<strong>piątą niedzielę
+                                Wielkiego Postu</strong>, tj.&nbsp;17&nbsp;marca br. Parafialna
+                                Spowiedź Wielkopostna w&nbsp;Uroczystość św.&nbsp;Józefa,
+                                tj.&nbsp;19&nbsp;marca br.</p>
+                                <p>3. W&nbsp;każdą niedzielę Wielkiego Postu nabożeństwo „Gorzkich
+                                Żali” z&nbsp;nauką pasyjną o&nbsp;godz.&nbsp;17.15, następnie Msza
+                                Święta.</p>
+                                <p>4. W&nbsp;ramach jałmużny wielkopostnej, dziś po&nbsp;Mszach św.
+                                zbiórka do&nbsp;puszek na&nbsp;rzecz dzieła pomocy misjonarzom
+                                „Ad&nbsp;Gentes”.</p>
+                                <p>5. W&nbsp;tym tygodniu Pierwszy piątek i&nbsp;Pierwsza sobota
+                                miesiąca. W&nbsp;Pierwszy Piątek Miesiąca, spowiedź w&nbsp;tym dniu
+                                od&nbsp;godz.&nbsp;17.00.</p>
+                                <p>6. W&nbsp;najbliższą sobotę o&nbsp;godz.&nbsp;17.15. – nabożeństwo
+                                Różańcowe poprowadzi Legion Maryi z&nbsp;naszej parafii.</p>
+                                <p>7. W&nbsp;przyszłą niedzielę w&nbsp;kościele, pół godziny przed
+                                Mszą&nbsp;św. o&nbsp;godz.&nbsp;11.15 – adoracja Najśw. Sakramentu
+                                połączona z&nbsp;modlitwą różańcową, którą poprowadzi Koło Żywego
+                                Różańca z&nbsp;naszej parafii i&nbsp;zmiana tajemnic różańcowych.
+                                Zapraszamy do&nbsp;udziału w&nbsp;tej modlitwie.</p>
+                                <p>8. Są&nbsp;jeszcze wolne miejsce w&nbsp;drugim autokarze
+                                na&nbsp;pielgrzymkę do&nbsp;Włoch. Szczegółowe informacje
+                                u&nbsp;ks.&nbsp;Krystiana Musiała.</p>
+                                <p>9. Zachęcamy do&nbsp;czytania prasy katolickiej: „Niedziela”, „Gość
+                                Niedzielny”.</p>
+                                <p>10. W&nbsp;zakrystii są&nbsp;do&nbsp;nabycia Baranki wielkanocne
+                                w&nbsp;cenie 8&nbsp;zł.</p>
+                                <p>11. Wszystkim uczestniczącym we&nbsp;Mszy&nbsp;św. życzymy
+                                błogosławionej niedzieli.</p>
+                                <p>
+                                    <span style="float: left;">Sandomierz, 25.&nbsp;lutego 2024&nbsp;r.</span>
+                                    <span style="float: right; text-align: right;">ks.&nbsp;proboszcz Stanisław Chmielewski</span>
+                                </p>
+                            </div>
+                        </div>
+                        <br>
+
+                        <div class="card">
+                            <div class="card-header">
                                 <h3>INTENCJE MSZY ŚWIĘTYCH<br>
                                     19.02. – 25.02.2024&nbsp;r.
                                 </h3>
--- last_post.php
+++ last_post.php
@@ -13,45 +13,49 @@
 
 <div class="card">
     <div class="card-header">
-        <h3>25.&nbsp;lutego 2024&nbsp;r.</h3>
-        <h3>II&nbsp;NIEDZIELA WIELKIEGO POSTU, Rok&nbsp;B</h3>
+        <h3>3.&nbsp;marca 2024&nbsp;r.</h3>
+        <h3>III&nbsp;NIEDZIELA WIELKIEGO POSTU, Rok&nbsp;B</h3>
     </div>
     <div class="col">
-        <p>1. Z&nbsp;racji, że&nbsp;w&nbsp;naszym kościele jest Stacyjna Droga
-        Krzyżowa, wyjątkowo, w&nbsp;najbliższy piątek Droga Krzyżowa
-        o&nbsp;godz.&nbsp;17.00 - zapraszamy do&nbsp;udziału.</p>
-        <p>2. <strong>Parafialne Rekolekcje Wielkopostne</strong>
-        w&nbsp;naszej parafii rozpoczną się w&nbsp;<strong>piątą niedzielę
-        Wielkiego Postu</strong>, tj.&nbsp;17&nbsp;marca br. Parafialna
-        Spowiedź Wielkopostna w&nbsp;Uroczystość św.&nbsp;Józefa,
-        tj.&nbsp;19&nbsp;marca br.</p>
-        <p>3. W&nbsp;każdą niedzielę Wielkiego Postu nabożeństwo „Gorzkich
-        Żali” z&nbsp;nauką pasyjną o&nbsp;godz.&nbsp;17.15, następnie Msza
-        Święta.</p>
-        <p>4. W&nbsp;ramach jałmużny wielkopostnej, dziś po&nbsp;Mszach św.
-        zbiórka do&nbsp;puszek na&nbsp;rzecz dzieła pomocy misjonarzom
-        „Ad&nbsp;Gentes”.</p>
-        <p>5. W&nbsp;tym tygodniu Pierwszy piątek i&nbsp;Pierwsza sobota
-        miesiąca. W&nbsp;Pierwszy Piątek Miesiąca, spowiedź w&nbsp;tym dniu
-        od&nbsp;godz.&nbsp;17.00.</p>
-        <p>6. W&nbsp;najbliższą sobotę o&nbsp;godz.&nbsp;17.15. – nabożeństwo
-        Różańcowe poprowadzi Legion Maryi z&nbsp;naszej parafii.</p>
-        <p>7. W&nbsp;przyszłą niedzielę w&nbsp;kościele, pół godziny przed
-        Mszą&nbsp;św. o&nbsp;godz.&nbsp;11.15 – adoracja Najśw. Sakramentu
-        połączona z&nbsp;modlitwą różańcową, którą poprowadzi Koło Żywego
-        Różańca z&nbsp;naszej parafii i&nbsp;zmiana tajemnic różańcowych.
-        Zapraszamy do&nbsp;udziału w&nbsp;tej modlitwie.</p>
-        <p>8. Są&nbsp;jeszcze wolne miejsce w&nbsp;drugim autokarze
-        na&nbsp;pielgrzymkę do&nbsp;Włoch. Szczegółowe informacje
-        u&nbsp;ks.&nbsp;Krystiana Musiała.</p>
-        <p>9. Zachęcamy do&nbsp;czytania prasy katolickiej: „Niedziela”, „Gość
-        Niedzielny”.</p>
-        <p>10. W&nbsp;zakrystii są&nbsp;do&nbsp;nabycia Baranki wielkanocne
+        <p>1. Droga Krzyżowa w&nbsp;każdy piątek Wielkiego Postu
+        o&nbsp;godz.&nbsp;17.15 - zapraszamy do&nbsp;udziału. W&nbsp;każdą
+        niedzielę Wielkiego Postu nabożeństwo „Gorzkich Żali” z&nbsp;nauką
+        pasyjną o&nbsp;godz.&nbsp;17.15, następnie Msza Święta. Za&nbsp;udział
+        w&nbsp;tych nabożeństwach, można zyskać odpust zupełny pod zwykłymi
+        warunkami.</p>
+        <p>2. Od&nbsp;drugiego semestru tego roku szkolnego zaczynają się
+        katechezy uzupełniające dla młodzieży pt.&nbsp;„Wierzę w&nbsp;Ciebie
+        Boże żywy”. Katechezy odbywają się w&nbsp;parafii św.&nbsp;Józefa,
+        najbliższa katecheza (9&nbsp;marca, tj.&nbsp;w&nbsp;sobotę
+        o&nbsp;godz.&nbsp;11.00).</p>
+        <p>3. Jest jeszcze kilka miejsc w&nbsp;drugim autokarze
+        na&nbsp;pielgrzymkę do&nbsp;Włoch. Wszystkie osoby, które się zapisały
+        na&nbsp;pielgrzymkę prosimy, aby należność w&nbsp;złotówkach
+        uregulować do&nbsp;końca marca, a&nbsp;w&nbsp;euro do&nbsp;końca
+        kwietnia.</p>
+        <p>4. Trwa Kurs Przedmałżeński w&nbsp;dekanacie sandomierskim,
+        w&nbsp;parafii św.&nbsp;Józefa w&nbsp;Sandomierzu, w&nbsp;niedzielę
+        o&nbsp;godz.&nbsp;16.30, spotkanie w&nbsp;salce katechetycznej przy
+        plebanii.</p>
+        <p>5. Spotkanie dla kandydatów do&nbsp;Bierzmowania z&nbsp;klas VIII
+        odbędzie się w&nbsp;tym tygodniu – we&nbsp;wtorek 5&nbsp;marca –
+        po&nbsp;Mszy świętej wieczorowej w&nbsp;kościele. Prosimy, aby
+        na&nbsp;to&nbsp;spotkanie młodzież przyszła z&nbsp;rodzicami.</p>
+        <p>6. W&nbsp;tym tygodniu Pierwszy Czwartek Miesiąca
+        o&nbsp;godz.&nbsp;17.30 zapraszamy na&nbsp;adorację Najświętszego
+        Sakramentu.</p>
+        <p>7. Wykonana została łazienka w&nbsp;kościele dla parafian. <em>Bóg
+        zapłać</em> Panom: Andrzejowi Anwajlerowi i&nbsp;Leszkowi Czajce
+        za&nbsp;pomoc w&nbsp;wykonaniu tego „<em>locum secretum</em>”.</p>
+        <p>8. Do&nbsp;nabycia w&nbsp;zakrystii Baranki wielkanocne
         w&nbsp;cenie 8&nbsp;zł.</p>
-        <p>11. Wszystkim uczestniczącym we&nbsp;Mszy&nbsp;św. życzymy
-        błogosławionej niedzieli.</p>
+        <p>9. Zachęcamy do&nbsp;nabycia i&nbsp;lektury katolickiej prasy.
+        Gazety na&nbsp;stoliku pod chórem.</p>
+        <p>10. Dziękujemy za&nbsp;modlitwę, wszelką pomoc, składane ofiary
+        na&nbsp;potrzeby parafii. Wszystkim uczestniczącym
+        we&nbsp;Mszy&nbsp;św. życzymy błogosławionej niedzieli.</p>
         <p>
-            <span style="float: left;">Sandomierz, 25.&nbsp;lutego 2024&nbsp;r.</span>
+            <span style="float: left;">Sandomierz, 3.&nbsp;marca 2024&nbsp;r.</span>
             <span style="float: right; text-align: right;">ks.&nbsp;proboszcz Stanisław Chmielewski</span>
         </p>
     </div>
