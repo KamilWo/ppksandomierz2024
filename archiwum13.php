eff2380b7400aa86023a29cb9b6ee74d35f392ee
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2024-06-09 22:06:28 +0100

    2024-06-09

--- archiwum13.php
+++ archiwum13.php
@@ -19,7 +19,146 @@ include 'head.php';
                     <!-- treść główna -->
                     <div class="col">
                         <h2 class="first-content-header">Archiwum wiadomości 2024 rok</h2>
-                        <h2>Okres 1.&nbsp;stycznia - 2.&nbsp;czerwca 2024&nbsp;r.</h2>
+                        <h2>Okres 1.&nbsp;stycznia - 9.&nbsp;czerwca 2024&nbsp;r.</h2>
+
+                        <div class="card">
+                            <div class="card-header">
+                                <h3>INTENCJE MSZY ŚWIĘTYCH<br>
+                                    03.06. – 09.06.2024&nbsp;r.
+                                </h3>
+                            </div>
+                            <div class="col">
+                        <table class="aktualnosci_table" cellpadding="5" cellspacing="0">
+                            <tbody>
+                                <tr class="actualnosci_day">
+                                    <td><b>PONIEDZIAŁEK, 3&nbsp;czerwca</b></td>
+                                    <td><b>Wspomnienie Św.&nbsp;Karola Langi
+                                    i&nbsp;Towarzyszy, męczenników</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. W&nbsp;intencji dzieci i&nbsp;chrześniaków
+                                    o&nbsp;żywą wiarę</td>
+                                </tr>
+                                <tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. + Agnieszkę Pietrusińską<br>
+                                    2. + Stanisławę, Jana, Stanisława, Witolda, Mariana,
+                                    Krystynę, Mariana i&nbsp;Janinę</td>
+                                </tr>
+
+                                <tr class="actualnosci_day">
+                                    <td><b>WTOREK, 4&nbsp;czerwca</b></td>
+                                    <td><b></b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. O&nbsp;łaskę dobrej śmierci<br>
+                                    2. + Teodorę Woźniak</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. + Jana Popka w rocz. śm.<br>
+                                    2. + Ludwikę Szczębarę, zm. z&nbsp;rodz. Szczębarów,
+                                    Golców, Królów, Ostrowskich, Bednarskich i&nbsp;dusze
+                                    w&nbsp;czyśćcu cierpiące</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>ŚRODA, 5&nbsp;czerwca</b></td>
+                                    <td><b>Wspomnienie Św.&nbsp;Bonifacego, biskupa
+                                    i&nbsp;męczennika</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. + Zbigniewa Kamińskiego 9&nbsp;rocz. śm., Ludwikę
+                                        i&nbsp;Jana Kamińskich<br>
+                                    2. + Stanisława Czosnka</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. + Roberta Mazura<br>
+                                    2. + Aleksandrę Ciamagę rocz. śm.</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>CZWARTEK, 6&nbsp;czerwca</b></td>
+                                    <td><b>I&nbsp;Czwartek miesiąca</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. O&nbsp;świętość kapłanów i&nbsp;nowe powołania
+                                        kapłańskie<br>
+                                    2. + Teodorę Woźniak</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. + Bogdana Płodowskiego<br>
+                                    2. + Barbarę i&nbsp;Mieczysława Bryłów, Bogusława Bryłę,
+                                    Andrzeja Jakubowskiego</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>PIĄTEK, 7&nbsp;czerwca</b></td>
+                                    <td><b>Uroczystość Najświętszego Serca Pana Jezusa<br>
+                                    I&nbsp;Piątek miesiąca</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. + Andrzeja Margulę<br>
+                                    2. + Teodorę Woźniak</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. + Tomasza 26&nbsp;rocz. śm.<br>
+                                    2. + Kamilę Załuską 18&nbsp;rocz. śm., Władysława
+                                    Pawłowskiego</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>SOBOTA, 8&nbsp;czerwca</b></td>
+                                    <td><b>Wspomnienie Niepokalanego Serca NMP</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. + Genowefę Kaczor 1&nbsp;rocz. śm.</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. Dziękczynno-błagalna o&nbsp;Boże błog. dla
+                                        Heleny<br>
+                                    2. + Jana i&nbsp;Andrzeja Popków</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>NIEDZIELA, 9&nbsp;czerwca</b></td>
+                                    <td><b>
+                                        <span style="color: red">X&nbsp;Niedziela Zwykła</span>
+                                    </b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">7.30</td>
+                                    <td>Za&nbsp;parafian</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">9.30</td>
+                                    <td>+ Zofię, Wawrzyńca Kwietniów, zm. z&nbsp;rodz.
+                                    Kwietniów i&nbsp;Kondeuszów</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">11.15</td>
+                                    <td>O&nbsp;Boże błog., porzebne łaski, opiekę MB dla Hani
+                                    z&nbsp;racji 16&nbsp;rocz. urodzin</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">12.30</td>
+                                    <td>+ Mariana w&nbsp;rocz. śm., Helenę Przyłuckich</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>+ Zofię Kołodziej</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                            </div>
+                        </div>
+                        <br>
 
                         <div class="card">
                             <div class="card-header">
--- last_intentions.php
+++ last_intentions.php
@@ -1,135 +1,130 @@
 <div class="card">
     <div class="card-header">
         <h3>INTENCJE MSZY ŚWIĘTYCH<br>
-            03.06. – 09.06.2024&nbsp;r.
+            10.06. – 16.06.2024&nbsp;r.
         </h3>
     </div>
     <div class="col">
 <table class="aktualnosci_table" cellpadding="5" cellspacing="0">
     <tbody>
         <tr class="actualnosci_day">
-            <td><b>PONIEDZIAŁEK, 3&nbsp;czerwca</b></td>
-            <td><b>Wspomnienie Św.&nbsp;Karola Langi
-            i&nbsp;Towarzyszy, męczenników</b></td>
+            <td><b>PONIEDZIAŁEK, 10&nbsp;czerwca</b></td>
+            <td><b></b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>1. W&nbsp;intencji dzieci i&nbsp;chrześniaków
-            o&nbsp;żywą wiarę</td>
+            <td>1. + Elżbietę Socha</td>
         </tr>
         <tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
             <td>1. + Agnieszkę Pietrusińską<br>
-            2. + Stanisławę, Jana, Stanisława, Witolda, Mariana,
-            Krystynę, Mariana i&nbsp;Janinę</td>
+            2. O&nbsp;doświadzenie Bożej miłości i&nbsp;łaskę
+            nawrócenia dla męża Piotra, i&nbsp;ojca Tadeusza</td>
         </tr>
-
         <tr class="actualnosci_day">
-            <td><b>WTOREK, 4&nbsp;czerwca</b></td>
-            <td><b></b></td>
+            <td><b>WTOREK, 11&nbsp;czerwca</b></td>
+            <td><b>Wspomnienie św.&nbsp;Barnaby, Apostoła</b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>1. O&nbsp;łaskę dobrej śmierci<br>
+            <td>1. + Irenę i&nbsp;Władysława Młynarczyków<br>
             2. + Teodorę Woźniak</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>1. + Jana Popka w rocz. śm.<br>
-            2. + Ludwikę Szczębarę, zm. z&nbsp;rodz. Szczębarów,
-            Golców, Królów, Ostrowskich, Bednarskich i&nbsp;dusze
-            w&nbsp;czyśćcu cierpiące</td>
+            <td>1. KWC<br>
+            2. + Helenę, Ludwika Wabiów w&nbsp;rocz. śm., i&nbsp;zm.
+            z&nbsp;rodz.</td>
         </tr>
         <tr class="actualnosci_day">
-            <td><b>ŚRODA, 5&nbsp;czerwca</b></td>
-            <td><b>Wspomnienie Św.&nbsp;Bonifacego, biskupa
-            i&nbsp;męczennika</b></td>
+            <td><b>ŚRODA, 12&nbsp;czerwca</b></td>
+            <td><b></b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>1. + Zbigniewa Kamińskiego 9&nbsp;rocz. śm., Ludwikę
-                i&nbsp;Jana Kamińskich<br>
-            2. + Stanisława Czosnka</td>
+            <td>1. + Kazimierę i&nbsp;Franciszka Musiałów<br>
+            2. + Teodorę Woźniak</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>1. + Roberta Mazura<br>
-            2. + Aleksandrę Ciamagę rocz. śm.</td>
+            <td>1. + Marka Stefaniaka<br>
+            2. + Adama Margiela 6&nbsp;rocz. śm.</td>
         </tr>
         <tr class="actualnosci_day">
-            <td><b>CZWARTEK, 6&nbsp;czerwca</b></td>
-            <td><b>I&nbsp;Czwartek miesiąca</b></td>
+            <td><b>CZWARTEK, 13&nbsp;czerwca</b></td>
+            <td><b>Wspomnienie św.&nbsp;Antoniego z&nbsp;Padwy,
+            Prezbitera i&nbsp;Doktora Kościoła</b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>1. O&nbsp;świętość kapłanów i&nbsp;nowe powołania
-                kapłańskie<br>
+            <td>1. + Stanisława i&nbsp;Idę Głazów<br>
+            2. + Teodorę Woźniak</td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>1. + Anielę i&nbsp;Antoniego Gospodarczyków<br>
+            2. + Irenę Borycką</td>
+        </tr>
+        <tr class="actualnosci_day">
+            <td><b>PIĄTEK, 14&nbsp;czerwca</b></td>
+            <td><b>Wspomnienie bł.&nbsp;Michała Kozala, Biskupa
+            i&nbsp;Męczennika</b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>1. + Stanisławę i&nbsp;Aleksandra Kowalów<br>
             2. + Teodorę Woźniak</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
             <td>1. + Bogdana Płodowskiego<br>
-            2. + Barbarę i&nbsp;Mieczysława Bryłów, Bogusława Bryłę,
-            Andrzeja Jakubowskiego</td>
+            2. + Stanisława Bartosza rocz. śm.</td>
         </tr>
         <tr class="actualnosci_day">
-            <td><b>PIĄTEK, 7&nbsp;czerwca</b></td>
-            <td><b>Uroczystość Najświętszego Serca Pana Jezusa<br>
-            I&nbsp;Piątek miesiąca</b></td>
+            <td><b>SOBOTA, 15&nbsp;czerwca</b></td>
+            <td><b></b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>1. + Andrzeja Margulę<br>
-            2. + Teodorę Woźniak</td>
+            <td>1. + Wiesława Napieracza</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>1. + Tomasza 26&nbsp;rocz. śm.<br>
-            2. + Kamilę Załuską 18&nbsp;rocz. śm., Władysława
-            Pawłowskiego</td>
+            <td>1. + Barbarę, Stanisława Stańków, Andrzeja
+                Stroińskiego<br>
+            2. O&nbsp;Boże błog., Dary Ducha Św., opiekę MB dla
+            Jolanty z&nbsp;okazji urodzin</td>
         </tr>
         <tr class="actualnosci_day">
-            <td><b>SOBOTA, 8&nbsp;czerwca</b></td>
-            <td><b>Wspomnienie Niepokalanego Serca NMP</b></td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">6.30</td>
-            <td>1. + Genowefę Kaczor 1&nbsp;rocz. śm.</td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">18.00</td>
-            <td>1. Dziękczynno-błagalna o&nbsp;Boże błog. dla
-                Heleny<br>
-            2. + Jana i&nbsp;Andrzeja Popków</td>
-        </tr>
-        <tr class="actualnosci_day">
-            <td><b>NIEDZIELA, 9&nbsp;czerwca</b></td>
+            <td><b>NIEDZIELA, 16&nbsp;czerwca</b></td>
             <td><b>
-                <span style="color: red">X&nbsp;Niedziela Zwykła</span>
+                <span style="color: red">XI&nbsp;Niedziela Zwykła</span>
             </b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">7.30</td>
-            <td>Za&nbsp;parafian</td>
+            <td>+ Mariannę, Franciszka, Henryka Banasiewiczów,
+            Stanisławę Asik</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">9.30</td>
-            <td>+ Zofię, Wawrzyńca Kwietniów, zm. z&nbsp;rodz.
-            Kwietniów i&nbsp;Kondeuszów</td>
+            <td>+ Roberta Burka, Janinę i&nbsp;Stefana Plachów</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">11.15</td>
-            <td>O&nbsp;Boże błog., porzebne łaski, opiekę MB dla Hani
-            z&nbsp;racji 16&nbsp;rocz. urodzin</td>
+            <td>DK</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">12.30</td>
-            <td>+ Mariana w&nbsp;rocz. śm., Helenę Przyłuckich</td>
+            <td>+ Bogusława Sarzyńskiego, Annę, Janusza, Stanisława
+            Roźmiejów, Zofię, Mikołaj, Eugeniusza Kapustów</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>+ Zofię Kołodziej</td>
+            <td>Dzięk.-błag. o&nbsp;Miłosierdzie Boże, wiarę, nadzieję
+            i&nbsp;miłość, łaskę pokory, nawrócenie i&nbsp;uzdrowienie
+            w&nbsp;rodzinie</td>
         </tr>
     </tbody>
 </table>
