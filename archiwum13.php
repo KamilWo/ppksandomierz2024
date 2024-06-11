ca3a6ba6859ea254b426fa03250250162399357c
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2024-06-11 21:48:40 +0100

    2024-06-09

--- archiwum13.php
+++ archiwum13.php
@@ -162,6 +162,83 @@ include 'head.php';
 
                         <div class="card">
                             <div class="card-header">
+                                <h3>2.&nbsp;czerwca 2024&nbsp;r.</h3>
+                                <h3>IX&nbsp;Niedziela Zwykła, Rok&nbsp;B</h3>
+                            </div>
+                            <div class="col">
+                                <p>1. Jesteśmy wdzięczni parafianom za&nbsp;przygotowanie pięknego
+                                ołtarza na&nbsp;Uroczystość Bożego Ciała w&nbsp;tym roku, dzieciom
+                                sypiącym kwiatki, niosącym chorągwie. Panu Andrzejowi Anwajlerowi
+                                za&nbsp;przygotowanie nagłośnienia procesji, Panu Aleksandrowi
+                                Stąporowi, właścicielowi firmy pogrzebowej, za&nbsp;skierowanie
+                                czterech pracowników do&nbsp;niesienia tub nagłaśniających
+                                w&nbsp;czasie procesji. Wyrażamy podziękowanie Panu Tadeuszowi
+                                Woźniakowi za&nbsp;pomoc, wszystkim, którzy w&nbsp;jakikolwiek sposób
+                                przyczynili się do&nbsp;tego, że&nbsp;mogliśmy przeżyć to&nbsp;święto
+                                godnie i&nbsp;pobożnie. <em>Bóg zapłać</em>. Także wszystkim
+                                parafianom, którzy wzięli udział tej Uroczystości bardzo
+                                dziękujemy.</p>
+                                <p>2. Dziękujemy rodzicom tegorocznych dzieci pierwszokomunijnym
+                                za&nbsp;posprzątanie kościoła, wykonanie kwiatowej dekoracji
+                                na&nbsp;uroczystości oraz za&nbsp;złożoną ofiarę na&nbsp;potrzeby
+                                parafii w&nbsp;wysokości 4&nbsp;tysięcy zł, na&nbsp;potrzeby parafii
+                                i&nbsp;upominki dla księży. Wszystkim <em>Bóg zapłać</em>.</p>
+                                <p>3. Przypominamy, że&nbsp;8&nbsp;czerwca, tj.&nbsp;w&nbsp;najbliższą
+                                sobotę, z&nbsp;Sandomierza do&nbsp;Rozwadowa przez Sokolniki, Orliska,
+                                Kotową Wolę przejedzie XV&nbsp;Rajd Papieski. Start godz.&nbsp;9.00 –
+                                Bulwar Józefa Piłsudskiego w&nbsp;Sandomierzu – meta Rozwadów –
+                                Sanktuarium Matki Bożej Szkaplerznej ok.&nbsp;godz.&nbsp;13.00.</p>
+                                <p>4. 8&nbsp;czerwca odbędzie się Diecezjalne Spotkanie Młodzieży
+                                w&nbsp;Sanktuarium Matki Bożej Szkaplerznej w&nbsp;Rozwadowie. Tego
+                                samego dnia natomiast z&nbsp;Sandomierza do&nbsp;Rozwadowa wyruszy
+                                XV&nbsp;Rajd Papieski. Szczegóły dotyczące rajdu znajdują się
+                                na&nbsp;stronie diecezjalnej.</p>
+                                <p>5. W&nbsp;tym tygodniu Pierwszy czwartek i&nbsp;piątek.
+                                W&nbsp;Pierwszy Piątek miesiąca, spowiedź w&nbsp;tym dniu
+                                od&nbsp;godz.&nbsp;17.30</p>
+                                <p>6. W&nbsp;czwartek zakończenie oktawy Bożego Ciała. Poświęcenie
+                                wianków i&nbsp;błogosławieństwo dzieci na&nbsp;zakończenie nabożeństwa
+                                eucharystycznego, które będzie po&nbsp;Mszy&nbsp;św.
+                                o&nbsp;godz.&nbsp;18.00.</p>
+                                <p>7. W&nbsp;najbliższy piątek Uroczystość Najświętszego Serca Pana
+                                Jezusa – Dzień Modlitw o&nbsp;Świętość Kapłanów (nie obowiązuje
+                                wstrzemięźliwość od&nbsp;pokarmów mięsnych.</p>
+                                <p>8. Dnia 15&nbsp;czerwca w&nbsp;Sandomierzu w&nbsp;ramach trwającego
+                                Roku Rodziny odbędzie się Diecezjalne spotkanie rodzin. Początek
+                                o&nbsp;godz.&nbsp;11 na&nbsp;Placu Papieskim, skąd w&nbsp;Marszu dla
+                                Życia i&nbsp;Rodziny przejdziemy pod Szkoły Katolickie im.
+                                św.&nbsp;Jadwigi Królowej, gdzie o&nbsp;godz.&nbsp;12.00 sprawowana
+                                będzie Msza&nbsp;św. Szczegóły na&nbsp;załączonym plakacie. Prosimy
+                                i&nbsp;zachęcamy naszych wiernych parafian do&nbsp;udziału
+                                w&nbsp;tym spotkaniu.</p>
+                                <p>9. W&nbsp;przyszłą niedzielę tradycyjnie zbiórka do&nbsp;puszek
+                                na&nbsp;Świątynie Opatrzności Bożej w&nbsp;ramach Dnia
+                                Dziękczynienia.</p>
+                                <p>10. Dnia 22&nbsp;czerwca w&nbsp;Domu Katolickim w&nbsp;Sandomierzu
+                                o&nbsp;godz.&nbsp;15, odbędzie się wręczenie Diecezjalnej Nagrody
+                                <em>Arbor bona</em>, połączone z&nbsp;koncertem Moniki Kuszyńskiej.
+                                Wstęp na&nbsp;koncert podobnie, jak w&nbsp;roku ubiegłym, jest
+                                bezpłatny. Zapraszanie wiernych na&nbsp;to&nbsp;wydarzenie.</p>
+                                <p>11. Zwolniło się jedno miejsce na&nbsp;pielgrzymkę do&nbsp;Włoch,
+                                osoby zainteresowane wyjazdem prosimy o&nbsp;kontakt
+                                z&nbsp;ks.&nbsp;Krystianem Musiałem. Spotkanie informacyjne dla
+                                wszystkich udających się na&nbsp;pielgrzymkę do&nbsp;Włoch odbędzie
+                                się w&nbsp;środę – 12&nbsp;czerwca o&nbsp;godz.&nbsp;19.00 w&nbsp;Sali
+                                na&nbsp;plebanii. Prosimy, aby na&nbsp;to&nbsp;spotkanie zabrać
+                                ze&nbsp;sobą dowód osobisty lub paszport.</p>
+                                <p>12. Dziękujemy parafianom za&nbsp;modlitwę, wszelką pomoc, składane
+                                ofiary na&nbsp;potrzeby parafii. Wszystkim życzymy błogosławionej
+                                Niedzieli.</p>
+                                <p>
+                                    <span style="float: left;">Sandomierz, 2.&nbsp;czerwca 2024&nbsp;r.</span>
+                                    <span style="float: right; text-align: right;">ks.&nbsp;proboszcz Stanisław Chmielewski</span>
+                                </p>
+                            </div>
+                        </div>
+                        <br>
+
+                        <div class="card">
+                            <div class="card-header">
                                 <h3>INTENCJE MSZY ŚWIĘTYCH<br>
                                     27.05. – 02.06.2024&nbsp;r.
                                 </h3>
--- last_post.php
+++ last_post.php
@@ -13,75 +13,47 @@
 
 <div class="card">
     <div class="card-header">
-        <h3>2.&nbsp;czerwca 2024&nbsp;r.</h3>
-        <h3>IX&nbsp;Niedziela Zwykła, Rok&nbsp;B</h3>
+        <h3>9.&nbsp;czerwca 2024&nbsp;r.</h3>
+        <h3>X&nbsp;Niedziela Zwykła, Rok&nbsp;B</h3>
     </div>
     <div class="col">
-        <p>1. Jesteśmy wdzięczni parafianom za&nbsp;przygotowanie pięknego
-        ołtarza na&nbsp;Uroczystość Bożego Ciała w&nbsp;tym roku, dzieciom
-        sypiącym kwiatki, niosącym chorągwie. Panu Andrzejowi Anwajlerowi
-        za&nbsp;przygotowanie nagłośnienia procesji, Panu Aleksandrowi
-        Stąporowi, właścicielowi firmy pogrzebowej, za&nbsp;skierowanie
-        czterech pracowników do&nbsp;niesienia tub nagłaśniających
-        w&nbsp;czasie procesji. Wyrażamy podziękowanie Panu Tadeuszowi
-        Woźniakowi za&nbsp;pomoc, wszystkim, którzy w&nbsp;jakikolwiek sposób
-        przyczynili się do&nbsp;tego, że&nbsp;mogliśmy przeżyć to&nbsp;święto
-        godnie i&nbsp;pobożnie. <em>Bóg zapłać</em>. Także wszystkim
-        parafianom, którzy wzięli udział tej Uroczystości bardzo
-        dziękujemy.</p>
-        <p>2. Dziękujemy rodzicom tegorocznych dzieci pierwszokomunijnym
-        za&nbsp;posprzątanie kościoła, wykonanie kwiatowej dekoracji
-        na&nbsp;uroczystości oraz za&nbsp;złożoną ofiarę na&nbsp;potrzeby
-        parafii w&nbsp;wysokości 4&nbsp;tysięcy zł, na&nbsp;potrzeby parafii
-        i&nbsp;upominki dla księży. Wszystkim <em>Bóg zapłać</em>.</p>
-        <p>3. Przypominamy, że&nbsp;8&nbsp;czerwca, tj.&nbsp;w&nbsp;najbliższą
-        sobotę, z&nbsp;Sandomierza do&nbsp;Rozwadowa przez Sokolniki, Orliska,
-        Kotową Wolę przejedzie XV&nbsp;Rajd Papieski. Start godz.&nbsp;9.00 –
-        Bulwar Józefa Piłsudskiego w&nbsp;Sandomierzu – meta Rozwadów –
-        Sanktuarium Matki Bożej Szkaplerznej ok.&nbsp;godz.&nbsp;13.00.</p>
-        <p>4. 8&nbsp;czerwca odbędzie się Diecezjalne Spotkanie Młodzieży
-        w&nbsp;Sanktuarium Matki Bożej Szkaplerznej w&nbsp;Rozwadowie. Tego
-        samego dnia natomiast z&nbsp;Sandomierza do&nbsp;Rozwadowa wyruszy
-        XV&nbsp;Rajd Papieski. Szczegóły dotyczące rajdu znajdują się
-        na&nbsp;stronie diecezjalnej.</p>
-        <p>5. W&nbsp;tym tygodniu Pierwszy czwartek i&nbsp;piątek.
-        W&nbsp;Pierwszy Piątek miesiąca, spowiedź w&nbsp;tym dniu
-        od&nbsp;godz.&nbsp;17.30</p>
-        <p>6. W&nbsp;czwartek zakończenie oktawy Bożego Ciała. Poświęcenie
-        wianków i&nbsp;błogosławieństwo dzieci na&nbsp;zakończenie nabożeństwa
-        eucharystycznego, które będzie po&nbsp;Mszy&nbsp;św.
-        o&nbsp;godz.&nbsp;18.00.</p>
-        <p>7. W&nbsp;najbliższy piątek Uroczystość Najświętszego Serca Pana
-        Jezusa – Dzień Modlitw o&nbsp;Świętość Kapłanów (nie obowiązuje
-        wstrzemięźliwość od&nbsp;pokarmów mięsnych.</p>
-        <p>8. Dnia 15&nbsp;czerwca w&nbsp;Sandomierzu w&nbsp;ramach trwającego
-        Roku Rodziny odbędzie się Diecezjalne spotkanie rodzin. Początek
-        o&nbsp;godz.&nbsp;11 na&nbsp;Placu Papieskim, skąd w&nbsp;Marszu dla
-        Życia i&nbsp;Rodziny przejdziemy pod Szkoły Katolickie im.
-        św.&nbsp;Jadwigi Królowej, gdzie o&nbsp;godz.&nbsp;12.00 sprawowana
-        będzie Msza&nbsp;św. Szczegóły na&nbsp;załączonym plakacie. Prosimy
-        i&nbsp;zachęcamy naszych wiernych parafian do&nbsp;udziału
-        w&nbsp;tym spotkaniu.</p>
-        <p>9. W&nbsp;przyszłą niedzielę tradycyjnie zbiórka do&nbsp;puszek
-        na&nbsp;Świątynie Opatrzności Bożej w&nbsp;ramach Dnia
-        Dziękczynienia.</p>
-        <p>10. Dnia 22&nbsp;czerwca w&nbsp;Domu Katolickim w&nbsp;Sandomierzu
-        o&nbsp;godz.&nbsp;15, odbędzie się wręczenie Diecezjalnej Nagrody
-        <em>Arbor bona</em>, połączone z&nbsp;koncertem Moniki Kuszyńskiej.
-        Wstęp na&nbsp;koncert podobnie, jak w&nbsp;roku ubiegłym, jest
-        bezpłatny. Zapraszanie wiernych na&nbsp;to&nbsp;wydarzenie.</p>
-        <p>11. Zwolniło się jedno miejsce na&nbsp;pielgrzymkę do&nbsp;Włoch,
-        osoby zainteresowane wyjazdem prosimy o&nbsp;kontakt
-        z&nbsp;ks.&nbsp;Krystianem Musiałem. Spotkanie informacyjne dla
-        wszystkich udających się na&nbsp;pielgrzymkę do&nbsp;Włoch odbędzie
-        się w&nbsp;środę – 12&nbsp;czerwca o&nbsp;godz.&nbsp;19.00 w&nbsp;Sali
+        <p>1. Trzynastego czerwca, tj.&nbsp;w&nbsp;czwartek, przypada rocznica
+        nominacji ks.&nbsp;dr&nbsp;Krzysztofa Nitkiewicza na&nbsp;Biskupa
+        Sandomierskiego w&nbsp;2009 roku, jest okazją do&nbsp;modlitwy
+        w&nbsp;intencji Pasterza Kościoła Sandomierskiego.</p>
+        <p>2. Spotkanie informacyjne dla wszystkich udających się
+        na&nbsp;pielgrzymkę do&nbsp;Włoch odbędzie się w&nbsp;środę –
+        <strong>12&nbsp;czerwca</strong> o&nbsp;godz.&nbsp;19.00 w&nbsp;Sali
         na&nbsp;plebanii. Prosimy, aby na&nbsp;to&nbsp;spotkanie zabrać
         ze&nbsp;sobą dowód osobisty lub paszport.</p>
-        <p>12. Dziękujemy parafianom za&nbsp;modlitwę, wszelką pomoc, składane
-        ofiary na&nbsp;potrzeby parafii. Wszystkim życzymy błogosławionej
-        Niedzieli.</p>
+        <p>3. Dnia 15&nbsp;czerwca w&nbsp;Sandomierzu, w&nbsp;ramach
+        trwającego Roku Rodziny, odbędzie się Diecezjalne spotkanie rodzin.
+        Początek o&nbsp;godz.&nbsp;11 na&nbsp;Placu Papieskim, skąd
+        w&nbsp;Marszu dla Życia i&nbsp;Rodziny przejdziemy pod Szkoły
+        Katolickie im.&nbsp;św.&nbsp;Jadwigi Królowej, gdzie
+        o&nbsp;godz.&nbsp;12.00 sprawowana będzie Msza&nbsp;św. Szczegóły
+        na&nbsp;załączonym plakacie. Prosimy i&nbsp;zachęcamy naszych wiernych
+        parafian do&nbsp;udziału w&nbsp;tym spotkaniu.</p>
+        <p>4. Dziś po&nbsp;zakończeniu Mszy&nbsp;Św. zbiórka do&nbsp;puszek
+        na&nbsp;Świątynie Opatrzności Bożej w&nbsp;ramach Dnia
+        Dziękczynienia.</p>
+        <p>5. Dnia 22&nbsp;czerwca, tj.&nbsp;sobota, w&nbsp;Domu Katolickim
+        w&nbsp;Sandomierzu o&nbsp;godz.&nbsp;15.00, odbędzie się wręczenie
+        Diecezjalnej Nagrody „Arbor bona”, połączone z&nbsp;koncertem Moniki
+        Kuszyńskiej. Wstęp na&nbsp;koncert podobnie, jak w&nbsp;roku ubiegłym,
+        jest bezpłatny. Zapraszamy wiernych na&nbsp;to&nbsp;wydarzenie.</p>
+        <p>6. Słowa wdzięczności wyrażamy rodzicom dzieci, które w&nbsp;zeszłą
+        niedzielę przeżywały rocznicę Pierwszej Komunii&nbsp;św. - ofiarowali
+        1000&nbsp;zł na&nbsp;potrzeby parafii. <em>Bóg zapłać</em>.</p>
+        <p>7. Prasa katolicka na&nbsp;stoliku, szczególnie polecamy artykuł
+        o&nbsp;proboszczach w&nbsp;czasopiśmie „Niedziela”.</p>
+        <p>8. Zmarł ks.&nbsp;Jan Toś pochodzący z&nbsp;tej parafii, przeżył
+        65&nbsp;lat, jutro godz.&nbsp;13.00 Msza&nbsp;św. żałobna w&nbsp;jego
+        intencji w&nbsp;naszym parafialnym kościele. <em>Wieczny odpoczynek
+        racz mu dać Panie …</em></p>
+        <p>9. Wszystkim życzymy błogosławionej Niedzieli.</p>
         <p>
-            <span style="float: left;">Sandomierz, 2.&nbsp;czerwca 2024&nbsp;r.</span>
+            <span style="float: left;">Sandomierz, 9.&nbsp;czerwca 2024&nbsp;r.</span>
             <span style="float: right; text-align: right;">ks.&nbsp;proboszcz Stanisław Chmielewski</span>
         </p>
     </div>
