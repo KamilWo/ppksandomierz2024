eb95fc72ddc39ccdbc96aa3edd5cee5a4995d838
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2024-07-13 21:47:57 +0100

    2024-07-14

--- archiwum13.php
+++ archiwum13.php
@@ -19,7 +19,184 @@ include 'head.php';
                     <!-- treść główna -->
                     <div class="col">
                         <h2 class="first-content-header">Archiwum wiadomości 2024 rok</h2>
-                        <h2>Okres 1.&nbsp;stycznia - 30.&nbsp;czerwca 2024&nbsp;r.</h2>
+                        <h2>Okres 1.&nbsp;stycznia - 14.&nbsp;lipca 2024&nbsp;r.</h2>
+
+                        <div class="card">
+                            <div class="card-header">
+                                <h3>INTENCJE MSZY ŚWIĘTYCH<br>
+                                    8.07. – 14.07.2024&nbsp;r.
+                                </h3>
+                            </div>
+                            <div class="col">
+                        <table class="aktualnosci_table" cellpadding="5" cellspacing="0">
+                            <tbody>
+                                <tr class="actualnosci_day">
+                                    <td><b>PONIEDZIAŁEK, 8&nbsp;lipca</b></td>
+                                    <td><b>Wspomnienie św.&nbsp;Jana z&nbsp;Dukli</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>+ Józefę, Jana Marców, Ryszarda Pietruszkę</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>+ Leokadię Wnuk 13&nbsp;rocz. śm.</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>WTOREK, 9&nbsp;lipca</b></td>
+                                    <td><b></b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>KWC</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. W&nbsp;int. Gabriela w&nbsp;18&nbsp;rocz. urodzin<br>
+                                    2. + Mieczysława i&nbsp;Genowefę Chałupczak</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>ŚRODA, 10&nbsp;lipca</b></td>
+                                    <td><b></b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>+ Genowefę i&nbsp;Andrzeja Wosiów, Stanisława Wróbla,
+                                    Mariannę Sendrowicz</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>W&nbsp;int. pielgrzymów pielgrzymki „W&nbsp;intencj
+                                    kapłanów” do&nbsp;Medjugorie o&nbsp;potrzebne łaski, Boże
+                                    błog. i&nbsp;opiekę MB</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>CZWARTEK, 11&nbsp;lipca</b></td>
+                                    <td><b>Święto św.&nbsp;Benedykta, Patrona Europy</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>+ Wiesława Napieracza</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>+ Krystynę w&nbsp;rocz. śm.</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>PIĄTEK, 12&nbsp;lipca</b></td>
+                                    <td><b>Wspomnienie św.&nbsp;Brunona Bonifacego
+                                    z&nbsp;Kwerfurtu</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>+ Franciszka rocz. śm., Stefanię Kowalskich, Marię
+                                    Woś, Wiesławę Wałcerz</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>+ Zygmunta, Józefa, Annę Szcześniaków, Jana
+                                    i&nbsp;Agnieszkę Wieczorków</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>SOBOTA, 13&nbsp;lipca</b></td>
+                                    <td><b>Wspomnienie Świętych pustelników Andrzeja Świerada
+                                    i&nbsp;Benedykta</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. + Roberta Mazura<br>
+                                    2. + Stanisława, Janinę, Zdzisława Kulów</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>+ Aleksandrę, Franciszka i&nbsp;Kazimierza Burków</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>NIEDZIELA, 14&nbsp;lipca</b></td>
+                                    <td><b>
+                                        <span style="color: red">XV&nbsp;Niedziela Zwykła</span>
+                                    </b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">7.30</td>
+                                    <td>Za&nbsp;parafan</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">9.30</td>
+                                    <td>+ Henrykę, Leona Sochów, Stefana Stępnia</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">11.15</td>
+                                    <td>+ Małgorzatę Rak 20&nbsp;rocz. śm., zm. z&nbsp;rodz.
+                                    Raków</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">12.30</td>
+                                    <td>+ Teresę Piorun 2&nbsp;rocz. śm.</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>+ Janusza Tworowskiego 2&nbsp;rocz. śm., zmarłych jego
+                                    rodziców</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                            </div>
+                        </div>
+                        <br>
+
+                        <div class="card">
+                            <div class="card-header">
+                                <h3>7.&nbsp;lipca 2024&nbsp;r.</h3>
+                                <h3>XIV&nbsp;Niedziela Zwykła, Rok&nbsp;B</h3>
+                            </div>
+                            <div class="col">
+                                <p>1. Przypominamy, że&nbsp;Wyższe Seminarium Duchowne
+                                w&nbsp;Sandomierzu przyjmuje zgłoszenia nowych kandydatów, którzy
+                                chcieliby rozpocząć formację i&nbsp;studia przygotowujące
+                                do&nbsp;sakramentu kapłaństwa. Chętni mogą zgłaszać się osobiście
+                                w&nbsp;siedzibie Seminarium Duchownego w&nbsp;Sandomierzu,
+                                ul.&nbsp;Żeromskiego 6. Możliwy jest także kontakt telefoniczny lub
+                                mailowy.</p>
+                                <p>2. Na&nbsp;stoliku pod chórem jest najnowsza prasa katolicka.
+                                Zachęcamy do&nbsp;nabywania i&nbsp;czytania prasy katolickiej.
+                                Są&nbsp;czasopisma: „Niedziela”, „Miłujcie się”, „Gość Niedzielny”.</p>
+                                <p>3. W&nbsp;przyszłą niedzielę w&nbsp;czasie Mszy Świętych będzie się
+                                z&nbsp;nami modlił zespół "Ta&nbsp;Ruta" z&nbsp;Ukrainy
+                                z&nbsp;Tarnopola. Oni Pomagają sierocińcowi w&nbsp;Tarnopolu
+                                i&nbsp;ludziom ze&nbsp;wschodu, którzy są&nbsp;szczególnie dotknięci
+                                wojną. Piszą w&nbsp;mailu: „W&nbsp;sierocińcu przebywają dzieci
+                                w&nbsp;wieku od&nbsp;jednego miesiąca do&nbsp;18&nbsp;lat i&nbsp;jest
+                                tych dzieci do&nbsp;150 osób. Prosimy o&nbsp;pomoc, czy to&nbsp;będą
+                                słodycze dla tych dzieci, lub jedzenie: konserwy, kasza, żywność
+                                zapakowana o&nbsp;wydłużonym okresie przydatności do&nbsp;spożycia,
+                                przybory szkolne, pieluchy, chemia. Będziemy mieli płyty
+                                i&nbsp;pamiątki, ze&nbsp;sprzedaży których pieniądze będą
+                                na&nbsp;paliwo, na&nbsp;samochód, kupimy coś z&nbsp;wyposażenia
+                                do&nbsp;domu dziecka.</p>
+                                <p>4. W&nbsp;dniach od&nbsp;4 do&nbsp;12 sierpnia br. odbędzie się
+                                41&nbsp;Piesza Pielgrzymka Ziemi Sandomierskiej na&nbsp;Jasną Górę pod
+                                hasłem <strong>„Rodzina Zakorzeniona w&nbsp;Chrystusie
+                                i&nbsp;Kościele”</strong>. Więcej szczegółowych informacji na&nbsp;ten
+                                temat w&nbsp;gablocie parafialnej.</p>
+                                <p>5. Ks.&nbsp;Biskup Ordynariusz zamianował ks.&nbsp;Wojciecha Zająca
+                                wikariuszem w&nbsp;naszej parafii. Ks.&nbsp;Wojciech uprzednio
+                                pracował w&nbsp;parafii św.&nbsp;Teresy od&nbsp;Dzieciątka Jezus
+                                w&nbsp;Godziszowie koło Janowa Lubelskiego. Życzymy nowemu
+                                Ks.&nbsp;Wikariuszowi wszystkiego dobrego w&nbsp;posługiwaniu
+                                w&nbsp;naszej parafii.</p>
+                                <p>6. Wszystkim, którzy w&nbsp;tym tygodniu obchodzą swoje imieniny,
+                                urodziny, inne rocznice, składamy najlepsze życzenia. Dziękujemy
+                                za&nbsp;modlitwę w&nbsp;intencji nas księży i&nbsp;za&nbsp;ofiary
+                                na&nbsp;rzecz parafii. Wszystkim uczestniczącym we&nbsp;Mszy&nbsp;św.
+                                życzymy błogosławionej niedzieli.</p>
+                                <p>
+                                    <span style="float: left;">Sandomierz, 7.&nbsp;lipca 2024&nbsp;r.</span>
+                                    <span style="float: right; text-align: right;">ks.&nbsp;proboszcz Stanisław Chmielewski</span>
+                                </p>
+                            </div>
+                        </div>
+                        <br>
 
                         <div class="card">
                             <div class="card-header">
--- last_intentions.php
+++ last_intentions.php
@@ -1,120 +1,124 @@
 <div class="card">
     <div class="card-header">
         <h3>INTENCJE MSZY ŚWIĘTYCH<br>
-            8.07. – 14.07.2024&nbsp;r.
+            15.07. – 21.07.2024&nbsp;r.
         </h3>
     </div>
     <div class="col">
 <table class="aktualnosci_table" cellpadding="5" cellspacing="0">
     <tbody>
         <tr class="actualnosci_day">
-            <td><b>PONIEDZIAŁEK, 8&nbsp;lipca</b></td>
-            <td><b>Wspomnienie św.&nbsp;Jana z&nbsp;Dukli</b></td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">6.30</td>
-            <td>+ Józefę, Jana Marców, Ryszarda Pietruszkę</td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">18.00</td>
-            <td>+ Leokadię Wnuk 13&nbsp;rocz. śm.</td>
-        </tr>
-        <tr class="actualnosci_day">
-            <td><b>WTOREK, 9&nbsp;lipca</b></td>
-            <td><b></b></td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">6.30</td>
-            <td>KWC</td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">18.00</td>
-            <td>1. W&nbsp;int. Gabriela w&nbsp;18&nbsp;rocz. urodzin<br>
-            2. + Mieczysława i&nbsp;Genowefę Chałupczak</td>
-        </tr>
-        <tr class="actualnosci_day">
-            <td><b>ŚRODA, 10&nbsp;lipca</b></td>
-            <td><b></b></td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">6.30</td>
-            <td>+ Genowefę i&nbsp;Andrzeja Wosiów, Stanisława Wróbla,
-            Mariannę Sendrowicz</td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">18.00</td>
-            <td>W&nbsp;int. pielgrzymów pielgrzymki „W&nbsp;intencj
-            kapłanów” do&nbsp;Medjugorie o&nbsp;potrzebne łaski, Boże
-            błog. i&nbsp;opiekę MB</td>
-        </tr>
-        <tr class="actualnosci_day">
-            <td><b>CZWARTEK, 11&nbsp;lipca</b></td>
-            <td><b>Święto św.&nbsp;Benedykta, Patrona Europy</b></td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">6.30</td>
-            <td>+ Wiesława Napieracza</td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">18.00</td>
-            <td>+ Krystynę w&nbsp;rocz. śm.</td>
-        </tr>
-        <tr class="actualnosci_day">
-            <td><b>PIĄTEK, 12&nbsp;lipca</b></td>
-            <td><b>Wspomnienie św.&nbsp;Brunona Bonifacego
-            z&nbsp;Kwerfurtu</b></td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">6.30</td>
-            <td>+ Franciszka rocz. śm., Stefanię Kowalskich, Marię
-            Woś, Wiesławę Wałcerz</td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">18.00</td>
-            <td>+ Zygmunta, Józefa, Annę Szcześniaków, Jana
-            i&nbsp;Agnieszkę Wieczorków</td>
-        </tr>
-        <tr class="actualnosci_day">
-            <td><b>SOBOTA, 13&nbsp;lipca</b></td>
-            <td><b>Wspomnienie Świętych pustelników Andrzeja Świerada
-            i&nbsp;Benedykta</b></td>
+            <td><b>PONIEDZIAŁEK, 15&nbsp;lipca</b></td>
+            <td><b>Wspomnienie Św.&nbsp;Bonawentury, Biskupa
+            i&nbsp;Doktora Kościoła</b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
             <td>1. + Roberta Mazura<br>
-            2. + Stanisława, Janinę, Zdzisława Kulów</td>
+            2. O&nbsp;Boże Błog., opiekę MB i&nbsp;zdrowie dla
+            Filipa</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>+ Aleksandrę, Franciszka i&nbsp;Kazimierza Burków</td>
+            <td>1. + Roberta Drozda w&nbsp;2&nbsp;rocz. śm.,
+            i&nbsp;zm. z&nbsp;rodz. Drozdów, i&nbsp;Ziółków</td>
         </tr>
         <tr class="actualnosci_day">
-            <td><b>NIEDZIELA, 14&nbsp;lipca</b></td>
+            <td><b>WTOREK, 16&nbsp;lipca</b></td>
+            <td><b>Wspomnienie NMP z&nbsp;Góry Karmel</b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>1. Z&nbsp;prośbą o&nbsp;Boże błog. i&nbsp;opiekę MB
+            dla Antoniego w&nbsp;14&nbsp;rocz. urodzin</td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>1. + Teodorę Woźniak<br>
+            2. Dziękczynna za&nbsp;otrzymane łaski od&nbsp;czcicieli
+            Maryi noszących szkaplerz</td>
+        </tr>
+        <tr class="actualnosci_day">
+            <td><b>ŚRODA, 17&nbsp;lipca</b></td>
+            <td><b></b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>1. + Roberta Mazura<br>
+            2. + Wojciecha Łukawskiego</td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>1. + Mariannę, Kazimierza, Henryka Kopców, Mariannę,
+            Walentego Grzesików</td>
+        </tr>
+        <tr class="actualnosci_day">
+            <td><b>CZWARTEK, 18&nbsp;lipca</b></td>
+            <td><b></b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>1. + Roberta Mazura</td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>1. + Stanisława Libickiego<br>
+            2. + Emilię i&nbsp;Edwarda Paluchów</td>
+        </tr>
+        <tr class="actualnosci_day">
+            <td><b>PIĄTEK, 19&nbsp;lipca</b></td>
+            <td><b></b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>1. + Roberta Mazura<br>
+            2. + Henryka Libickiego</td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>1. + Edmunda, Helenę, Jana Szafrańskich, Teofilę,
+            Walentego Adamców</td>
+        </tr>
+        <tr class="actualnosci_day">
+            <td><b>SOBOTA, 20&nbsp;lipca</b></td>
+            <td><b></b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>1. + Mariana Sapielaka</td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>1. O&nbsp;Boże błog., dary Ducha Św., opiekę MB dla
+            Antoniego w&nbsp;14&nbsp;rocz. urodzin</td>
+        </tr>
+        <tr class="actualnosci_day">
+            <td><b>NIEDZIELA, 21&nbsp;lipca</b></td>
             <td><b>
-                <span style="color: red">XV&nbsp;Niedziela Zwykła</span>
+                <span style="color: red">XVI&nbsp;Niedziela Zwykła</span>
             </b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">7.30</td>
-            <td>Za&nbsp;parafan</td>
+            <td>O&nbsp;Boże błog., potrzebne łaski, opiekę MB dla
+            Beaty i&nbsp;syna Jakuba – dzięk-błag.</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">9.30</td>
-            <td>+ Henrykę, Leona Sochów, Stefana Stępnia</td>
+            <td>Za&nbsp;parafian</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">11.15</td>
-            <td>+ Małgorzatę Rak 20&nbsp;rocz. śm., zm. z&nbsp;rodz.
-            Raków</td>
+            <td>DK</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">12.30</td>
-            <td>+ Teresę Piorun 2&nbsp;rocz. śm.</td>
+            <td>+ Otylię, Juliusza Nagórnych, zm. z&nbsp;rodz.
+            Grzesiaków i&nbsp;Nagórnych</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>+ Janusza Tworowskiego 2&nbsp;rocz. śm., zmarłych jego
-            rodziców</td>
+            <td>+ Mariannę, Jakuba i&nbsp;Mieczysława Pijewskich</td>
         </tr>
     </tbody>
 </table>
