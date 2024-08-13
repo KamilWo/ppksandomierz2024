9e04a6e0e6d37f4fc1ba1483765a6687d4615bc6
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2024-08-13 07:21:19 +0100

    2024-08-12

--- last_post.php
+++ last_post.php
@@ -1,5 +1,81 @@
 <div class="card">
     <div class="card-header">
+        <h3>26-29&nbsp;września 2024&nbsp;r.</h3>
+        <h3>PIELGRZYMKA ŚLADAMI BOŻEGO MIŁOSIERDZIA<br>
+        WILNO – KOWNO – TROKI</h3>
+    </div>
+    <div class="col">
+        <a href="/static/download/2024_PIELGRZYMKA_ŚLADAMI_BOŻEGO_MIŁOSIERDZIA.pdf">
+            <img style="width: 100%;"
+                 src="/static/src/images/2024_Wilno_Kowno_Troki.jpg" />
+        </a>
+        <br><br>
+
+        <p><strong>1 dzień</strong> Wyjazd z&nbsp;miejsca zbiórki
+        w&nbsp;godzinach nocnych (2:30). Przejazd
+        do&nbsp;<strong>Sokółki</strong>, nawiedzenie
+        kościoła p.w.&nbsp;Św.&nbsp;Antoniego Padewskiego - miejsca
+        cudu eucharystycznego - Msza&nbsp;Św. Wieczorem przyjazd
+        do&nbsp;hotelu w&nbsp;<strong>Kownie</strong>, zakwaterowanie,
+        obiadokolacja, nocleg.</p><br>
+
+        <p><strong>2 dzień</strong> Po&nbsp;śniadaniu, zwiedzanie
+        miasta: Zamek, Ratusz, Katedra Św.&nbsp;Piotra i&nbsp;Pawła,
+        Dom Perkuna. Następnie przejazd do&nbsp;<strong>Wilna</strong>,
+        zwiedzanie miasta: Góra Trzech Krzyży, Kościół Św.&nbsp;Piotra
+        i&nbsp;Pawła, Ostra Brama - symbol miasta, jedyna zachowana
+        brama miejska ze&nbsp;średniowiecznych murów obronnych
+        (w&nbsp;jednej z&nbsp;kapliczek znajduje się cudowny obraz
+        Matki Boskiej - najważniejszy symbol religijny Litwy) -
+        Msza&nbsp;Św., Klasztor Bazylianów, w&nbsp;którym
+        w&nbsp;czasach zaboru rosyjskiego mieściło się więzienie
+        w&nbsp;jednej z&nbsp;cel nazwanej później „Celą Konrada” przez
+        6&nbsp;miesięcy był więziony Adam Mickiewicz, Ratusz, getto
+        żydowskie. Czas wolny do&nbsp;dyspozycji turystów.<br>
+        Wieczorem zakwaterowanie w&nbsp;hotelu, obiadokolacja, nocleg.</p><br>
+
+        <p><strong>3 dzień</strong> Po&nbsp;śniadaniu cd. zwiedzania
+        miasta: Cmentarz na&nbsp;Rosie, Uniwersytet, Katedra
+        z&nbsp;kaplicą z&nbsp;relikwiami św.&nbsp;Kazimierza – patrona
+        Litwy Kościół Św.&nbsp;Ducha, Sanktuarium gdzie znajduje się
+        oryginał obrazu Miłosierdzia Bożego „Jezu, ufam Tobie” -
+        Msza&nbsp;Św., Zamek Górny na&nbsp;Wzgórzu Gedymina. Czas
+        wolny do&nbsp;dyspozycji turystów. Wieczorem powrót
+        do&nbsp;hotelu, obiadokolacja, nocleg.</p><br>
+
+        <p><strong>4 dzień</strong> Po&nbsp;śniadaniu wyjazd
+        do&nbsp;<strong>Trok</strong> - dawnej stolicy Wielkiego
+        Księstwa Litewskiego, zwiedzanie miasteczka malowniczo
+        położonego nad jeziorem. Spacer wzdłuż ulicy Karaimskiej,
+        Kenesa – świątynia Karaimów - mniejszości etnicznej
+        pochodzenia tureckiego. Zapoznanie się z&nbsp;kulturą Karaimów
+        licznie zamieszkujących te&nbsp;tereny. Zwiedzanie położonego
+        na&nbsp;wyspie XIV - wiecznego zamku księcia Witolda.
+        Po&nbsp;zwiedzaniu czas wolny, możliwość posmakowania głównego
+        dania kuchni karaimskiej – kibinów. Msza&nbsp;Św.
+        w&nbsp;Sanktuarium Matki Boskiej Trockiej. Po południu
+        wyjazd w&nbsp;drogę powrotną.</p>
+        <p>Przyjazd na&nbsp;miejsce zbiórki w&nbsp;późnych godzinach
+        wieczornych.</p>
+
+        <h4>ODPŁATNOŚĆ: 1600&nbsp;zł. (grupa 50&nbsp;osób)</h4>
+        <h4>TERMIN: 26-29 września 2024 r.</h4>
+        <p>
+            <a href="/static/download/2024_PIELGRZYMKA_ŚLADAMI_BOŻEGO_MIŁOSIERDZIA.pdf">Plik do&nbsp;pobrania</a>
+            - dodatkowe informacje
+        </p>
+        <p>Szczegółowych informacji udziela i&nbsp;zapisy przyjmuje
+        ks.&nbsp;Krystian Musiał (+48 539-921-434).</p>
+        <p>
+            <span style="float: left;">Sandomierz, 12.&nbsp;sierpnia 2024&nbsp;r.</span>
+            <span style="float: right; text-align: right;">ks.&nbsp;proboszcz Stanisław Chmielewski</span>
+        </p>
+    </div>
+</div>
+<br>
+
+<div class="card">
+    <div class="card-header">
         <h3>14.&nbsp;lipca 2024&nbsp;r.</h3>
         <h3>XV&nbsp;Niedziela Zwykła, Rok&nbsp;B</h3>
     </div>
