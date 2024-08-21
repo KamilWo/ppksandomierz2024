e111511e780bf7008c370a109abd2279a094a3b3
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2024-08-21 18:19:25 +0100

    2024-08-18

--- archiwum13.php
+++ archiwum13.php
@@ -151,6 +151,53 @@ include 'head.php';
 
                         <div class="card">
                             <div class="card-header">
+                                <h3>14.&nbsp;lipca 2024&nbsp;r.</h3>
+                                <h3>XV&nbsp;Niedziela Zwykła, Rok&nbsp;B</h3>
+                            </div>
+                            <div class="col">
+                                <p>1. Na&nbsp;stoliku pod chórem jest najnowsza prasa katolicka.
+                                Zachęcamy do&nbsp;nabywania i&nbsp;czytania prasy katolickiej.
+                                Są&nbsp;czasopisma „Niedziela”, „Miłujcie się”, „Gość Niedzielny”.</p>
+                                <p>2. Dziś w&nbsp;czasie Mszy Świętych modli się z&nbsp;nami zespół
+                                "Ta&nbsp;Ruta" z&nbsp;Ukrainy z&nbsp;Tarnopola. Oni Pomagają
+                                sierocińcowi w&nbsp;Tarnopolu i&nbsp;ludziom ze&nbsp;wschodu, którzy
+                                są&nbsp;szczególnie dotknięci wojną. W&nbsp;sierocińcu przebywają
+                                dzieci w&nbsp;wieku od&nbsp;jednego miesiąca do&nbsp;18&nbsp;lat
+                                i&nbsp;jest tych dzieci do&nbsp;150 osób. Prosimy i&nbsp;dziękujemy
+                                za&nbsp;pomoc, czy to&nbsp;będą słodycze dla tych dzieci, lub
+                                jedzenie: konserwy, kasza, żywność zapakowana o&nbsp;wydłużonym
+                                okresie przydatności do&nbsp;spożycia, przybory szkolne, pieluchy,
+                                chemia. W&nbsp;przedsionku kościoła można nabyć płyty i&nbsp;pamiątki,
+                                kupując je&nbsp;wspieramy wspomniany dom dziecka.&nbsp;</p>
+                                <p>3. W&nbsp;dniach od&nbsp;4 do&nbsp;12&nbsp;sierpnia br. odbędzie
+                                się 41&nbsp;Piesza Pielgrzymka Ziemi Sandomierskiej na&nbsp;Jasną Górę
+                                pod hasłem <strong>„Rodzina Zakorzeniona w&nbsp;Chrystusie
+                                i&nbsp;Kościele”</strong>. Więcej szczegółowych informacji na&nbsp;ten
+                                temat w&nbsp;gablocie parafialnej.</p>
+                                <p>4. We&nbsp;wtorek, tj.&nbsp;16&nbsp;lipca br. w&nbsp;liturgii
+                                obchodzimy wspomnienie Najświętszej Maryi Panny z&nbsp;Góry Karmel.
+                                Zapraszamy wszystkich, którzy noszą Szkaplerz do&nbsp;udziału
+                                we&nbsp;Mszy Świętej o&nbsp;godzinie 18.00. Zapraszamy także chętnych
+                                do&nbsp;włączenia się do&nbsp;Rodziny Szkaplerznej, przyjęcia
+                                i&nbsp;noszenia na&nbsp;swojej piersi Szkaplerza. W&nbsp;czasie Mszy
+                                Świętej będziemy dziękować za&nbsp;otrzymane łaski i&nbsp;prosić
+                                o&nbsp;Boże błogosławieństwo przez pośrednictwo Maryi z&nbsp;Góry
+                                Karmel.</p>
+                                <p>5. Wszystkim, którzy w&nbsp;tym tygodniu obchodzą swoje imieniny,
+                                urodziny, inne rocznice, składamy najlepsze życzenia. Dziękujemy
+                                za&nbsp;modlitwę w&nbsp;intencji nas księży i&nbsp;za&nbsp;ofiary
+                                na&nbsp;rzecz parafii. Wszystkim uczestniczącym we&nbsp;Mszy&nbsp;św.
+                                życzymy błogosławionej niedzieli.</p>
+                                <p>
+                                    <span style="float: left;">Sandomierz, 14.&nbsp;lipca 2024&nbsp;r.</span>
+                                    <span style="float: right; text-align: right;">ks.&nbsp;proboszcz Stanisław Chmielewski</span>
+                                </p>
+                            </div>
+                        </div>
+                        <br>
+
+                        <div class="card">
+                            <div class="card-header">
                                 <h3>INTENCJE MSZY ŚWIĘTYCH<br>
                                     8.07. – 14.07.2024&nbsp;r.
                                 </h3>
--- last_post.php
+++ last_post.php
@@ -76,45 +76,30 @@
 
 <div class="card">
     <div class="card-header">
-        <h3>14.&nbsp;lipca 2024&nbsp;r.</h3>
-        <h3>XV&nbsp;Niedziela Zwykła, Rok&nbsp;B</h3>
+        <h3>18.&nbsp;sierpnia 2024&nbsp;r.</h3>
+        <h3>XX&nbsp;Niedziela Zwykła, Rok&nbsp;B</h3>
     </div>
     <div class="col">
-        <p>1. Na&nbsp;stoliku pod chórem jest najnowsza prasa katolicka.
-        Zachęcamy do&nbsp;nabywania i&nbsp;czytania prasy katolickiej.
+        <p>1. W&nbsp;sobotę dnia 24&nbsp;sierpnia br. Odbędzie się Diecezjalna
+        Piesza Pielgrzymka Trzeźwościowa z&nbsp;Sandomierza do&nbsp;Radomyśla.
+        Wyjście z&nbsp;katedry o&nbsp;godz.&nbsp;5.30. Więcej informacji pod
+        tel. 660&nbsp;329&nbsp;982.</p>
+        <p>2. Zachęcamy do&nbsp;modlitwy w&nbsp;intencji osób uzależnionych
+        od&nbsp;alkoholu i&nbsp;ich rodzin. Miesiąc sierpień w&nbsp;tradycji
+        Kościoła w&nbsp;Polsce, jest okresem szczególnej modlitwy
+        o&nbsp;trzeźwość w&nbsp;rodzinach.</p>
+        <p>3. Autokarowa pielgrzymka śladami Bożego Miłosierdzia
+        do&nbsp;Sokółki, Kowna i&nbsp;Wilna od&nbsp;26-29&nbsp;września br.
+        Koszt wyjazdu 1600&nbsp;zł plus 45&nbsp;euro. Plan pielgrzymki jest
+        w&nbsp;parafialnej gablocie. Szczegółowych informacji udziela
+        ks.&nbsp;Krystian Musiał.</p>
+        <p>4. Zachęcamy do&nbsp;nabywania i&nbsp;czytania prasy katolickiej.
         Są&nbsp;czasopisma „Niedziela”, „Miłujcie się”, „Gość Niedzielny”.</p>
-        <p>2. Dziś w&nbsp;czasie Mszy Świętych modli się z&nbsp;nami zespół
-        "Ta&nbsp;Ruta" z&nbsp;Ukrainy z&nbsp;Tarnopola. Oni Pomagają
-        sierocińcowi w&nbsp;Tarnopolu i&nbsp;ludziom ze&nbsp;wschodu, którzy
-        są&nbsp;szczególnie dotknięci wojną. W&nbsp;sierocińcu przebywają
-        dzieci w&nbsp;wieku od&nbsp;jednego miesiąca do&nbsp;18&nbsp;lat
-        i&nbsp;jest tych dzieci do&nbsp;150 osób. Prosimy i&nbsp;dziękujemy
-        za&nbsp;pomoc, czy to&nbsp;będą słodycze dla tych dzieci, lub
-        jedzenie: konserwy, kasza, żywność zapakowana o&nbsp;wydłużonym
-        okresie przydatności do&nbsp;spożycia, przybory szkolne, pieluchy,
-        chemia. W&nbsp;przedsionku kościoła można nabyć płyty i&nbsp;pamiątki,
-        kupując je&nbsp;wspieramy wspomniany dom dziecka.&nbsp;</p>
-        <p>3. W&nbsp;dniach od&nbsp;4 do&nbsp;12&nbsp;sierpnia br. odbędzie
-        się 41&nbsp;Piesza Pielgrzymka Ziemi Sandomierskiej na&nbsp;Jasną Górę
-        pod hasłem <strong>„Rodzina Zakorzeniona w&nbsp;Chrystusie
-        i&nbsp;Kościele”</strong>. Więcej szczegółowych informacji na&nbsp;ten
-        temat w&nbsp;gablocie parafialnej.</p>
-        <p>4. We&nbsp;wtorek, tj.&nbsp;16&nbsp;lipca br. w&nbsp;liturgii
-        obchodzimy wspomnienie Najświętszej Maryi Panny z&nbsp;Góry Karmel.
-        Zapraszamy wszystkich, którzy noszą Szkaplerz do&nbsp;udziału
-        we&nbsp;Mszy Świętej o&nbsp;godzinie 18.00. Zapraszamy także chętnych
-        do&nbsp;włączenia się do&nbsp;Rodziny Szkaplerznej, przyjęcia
-        i&nbsp;noszenia na&nbsp;swojej piersi Szkaplerza. W&nbsp;czasie Mszy
-        Świętej będziemy dziękować za&nbsp;otrzymane łaski i&nbsp;prosić
-        o&nbsp;Boże błogosławieństwo przez pośrednictwo Maryi z&nbsp;Góry
-        Karmel.</p>
-        <p>5. Wszystkim, którzy w&nbsp;tym tygodniu obchodzą swoje imieniny,
-        urodziny, inne rocznice, składamy najlepsze życzenia. Dziękujemy
-        za&nbsp;modlitwę w&nbsp;intencji nas księży i&nbsp;za&nbsp;ofiary
-        na&nbsp;rzecz parafii. Wszystkim uczestniczącym we&nbsp;Mszy&nbsp;św.
-        życzymy błogosławionej niedzieli.</p>
+        <p>5. Dziękujemy parafianom za&nbsp;modlitwę, wszelką pomoc, składane
+        ofiary na&nbsp;potrzeby parafii. Wszystkim życzymy błogosławionej
+        Niedzieli.</p>
         <p>
-            <span style="float: left;">Sandomierz, 14.&nbsp;lipca 2024&nbsp;r.</span>
+            <span style="float: left;">Sandomierz, 18.&nbsp;sierpnia 2024&nbsp;r.</span>
             <span style="float: right; text-align: right;">ks.&nbsp;proboszcz Stanisław Chmielewski</span>
         </p>
     </div>
