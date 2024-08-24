fca3406ca5530de0214424574fac804886a72b16
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2024-08-24 20:10:50 +0100

    2024-08-25

--- archiwum13.php
+++ archiwum13.php
@@ -19,7 +19,127 @@ include 'head.php';
                     <!-- treść główna -->
                     <div class="col">
                         <h2 class="first-content-header">Archiwum wiadomości 2024 rok</h2>
-                        <h2>Okres 1.&nbsp;stycznia - 21.&nbsp;lipca 2024&nbsp;r.</h2>
+                        <h2>Okres 1.&nbsp;stycznia - 25.&nbsp;sierpnia 2024&nbsp;r.</h2>
+
+                        <div class="card">
+                            <div class="card-header">
+                                <h3>INTENCJE MSZY ŚWIĘTYCH<br>
+                                    19.08. – 25.08.2024&nbsp;r.
+                                </h3>
+                            </div>
+                            <div class="col">
+                        <table class="aktualnosci_table" cellpadding="5" cellspacing="0">
+                            <tbody>
+                                <tr class="actualnosci_day">
+                                    <td><b>PONIEDZIAŁEK, 19&nbsp;sierpnia</b></td>
+                                    <td><b></b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>+ Ryszarda Rowińskiego</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>+ Jerzego Gawrońskiego, Stanisława, Stefanię Kołodziejów</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>WTOREK, 20&nbsp;sierpnia</b></td>
+                                    <td><b>Wspomnienie Św.&nbsp;Bernarda opata i&nbsp;Doktora
+                                    Kościoła</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>+ Ryszarda Rowińskiego</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>+ Mariana Sapielaka</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>ŚRODA, 21&nbsp;sierpnia</b></td>
+                                    <td><b>Wspomnienie Św.&nbsp;Piusa X, papieża</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>+ Ryszarda Rowińskiego</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>+ Roberta Mazura</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>CZWARTEK, 22&nbsp;sierpnia</b></td>
+                                    <td><b>Wspomnienie NMP Królowej</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>+ Andrzeja Kijanię – 20&nbsp;rocz. śm.</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>+ Sławomira Rykowskiego w&nbsp;rocz. śm.</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>PIĄTEK, 23&nbsp;sierpnia</b></td>
+                                    <td><b></b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>+ Piotra Stupczy – 2&nbsp;rocz. śm., Franciszka
+                                    Stypczy, Józefa i&nbsp;Krystynę Gajdów</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. O&nbsp;Boże Błog. dla Krzysztofa z&nbsp;okazji
+                                        50&nbsp;urodzin<br>
+                                        2. + Mariannę, Henryka, Józefa Surowców, Jana
+                                        Kędzierskiego</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>SOBOTA, 24&nbsp;sierpnia</b></td>
+                                    <td><b>Święto Św.&nbsp;Bartłomieja Apostoła</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. + Wiesława Napieracza<br>
+                                    2. Dzięk-błag. o&nbsp;Boże Błog., potrzebne łaski dla
+                                    Michała i&nbsp;Gabriela</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td></td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>NIEDZIELA, 25&nbsp;sierpnia</b></td>
+                                    <td><b>
+                                        <span style="color: red">XXI&nbsp;Niedziela Zwykła</span>
+                                    </b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">7.30</td>
+                                    <td>+ Zofię, Mariana Krawczyków, Józefa, Mariannę Mądrych</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">9.30</td>
+                                    <td>+ Gustawa Polita – 8&nbsp;rocz. śm.</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">11.15</td>
+                                    <td>Za&nbsp;parafian</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">12.30</td>
+                                    <td>+ Stanisławę i&nbsp;Henryka Krakowiaków</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>+ Roberta Mazura</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                            </div>
+                        </div>
+                        <br>
 
                         <div class="card">
                             <div class="card-header">
--- last_intentions.php
+++ last_intentions.php
@@ -1,116 +1,124 @@
 <div class="card">
     <div class="card-header">
         <h3>INTENCJE MSZY ŚWIĘTYCH<br>
-            19.08. – 25.08.2024&nbsp;r.
+            25.08. – 1.09.2024&nbsp;r.
         </h3>
     </div>
     <div class="col">
 <table class="aktualnosci_table" cellpadding="5" cellspacing="0">
     <tbody>
         <tr class="actualnosci_day">
-            <td><b>PONIEDZIAŁEK, 19&nbsp;sierpnia</b></td>
-            <td><b></b></td>
+            <td><b>PONIEDZIAŁEK, 26&nbsp;sierpnia</b></td>
+            <td><b>Uroczystość NMP Częstochowskiej</b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>+ Ryszarda Rowińskiego</td>
+            <td>+ Edwarda Ziębę</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>+ Jerzego Gawrońskiego, Stanisława, Stefanię Kołodziejów</td>
+            <td>Dziękczynna z&nbsp;racji 46&nbsp;rocz. ślubu:
+            Stanisławy i&nbsp;Stanisława Niezgodów</td>
         </tr>
         <tr class="actualnosci_day">
-            <td><b>WTOREK, 20&nbsp;sierpnia</b></td>
-            <td><b>Wspomnienie Św.&nbsp;Bernarda opata i&nbsp;Doktora
-            Kościoła</b></td>
+            <td><b>WTOREK, 27&nbsp;sierpnia</b></td>
+            <td><b>Wspomnienie św.&nbsp;Moniki</b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>+ Ryszarda Rowińskiego</td>
+            <td>1. + Mariana Sapielaka<br>
+            2. + Henryka Libickiego</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>+ Mariana Sapielaka</td>
+            <td>+ Adama Ozdobę</td>
         </tr>
         <tr class="actualnosci_day">
-            <td><b>ŚRODA, 21&nbsp;sierpnia</b></td>
-            <td><b>Wspomnienie Św.&nbsp;Piusa X, papieża</b></td>
+            <td><b>ŚRODA, 28&nbsp;sierpnia</b></td>
+            <td><b>Wspomnienie św.&nbsp;Augustyna, biskupa
+            i&nbsp;Doktora Kościoła</b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>+ Ryszarda Rowińskiego</td>
+            <td>1. + Meczysława Kunkiewicza<br>
+            2. + Henryka Libickiego</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
             <td>+ Roberta Mazura</td>
         </tr>
         <tr class="actualnosci_day">
-            <td><b>CZWARTEK, 22&nbsp;sierpnia</b></td>
-            <td><b>Wspomnienie NMP Królowej</b></td>
+            <td><b>CZWARTEK, 29&nbsp;sierpnia</b></td>
+            <td><b>Wspomnenie Męczeństwa św.&nbsp;Jana
+            Chrzciciela</b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>+ Andrzeja Kijanię – 20&nbsp;rocz. śm.</td>
+            <td>+ Edwarda Ziębę</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>+ Sławomira Rykowskiego w&nbsp;rocz. śm.</td>
+            <td>+ Roberta Mazura</td>
         </tr>
         <tr class="actualnosci_day">
-            <td><b>PIĄTEK, 23&nbsp;sierpnia</b></td>
+            <td><b>PIĄTEK, 30&nbsp;sierpnia</b></td>
             <td><b></b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>+ Piotra Stupczy – 2&nbsp;rocz. śm., Franciszka
-            Stypczy, Józefa i&nbsp;Krystynę Gajdów</td>
+            <td>+ Emilię, Szczepana Dudków, zm. z&nbsp;rodz., dusze
+            w&nbsp;czyśćcu cierpiące</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>1. O&nbsp;Boże Błog. dla Krzysztofa z&nbsp;okazji
-                50&nbsp;urodzin<br>
-                2. + Mariannę, Henryka, Józefa Surowców, Jana
-                Kędzierskiego</td>
+            <td>1. + Kazimierza Karę<br>
+            2. + Jana, Klarę, Ryszarda Chrząstowskich, Jana
+            Makowskiego</td>
         </tr>
         <tr class="actualnosci_day">
-            <td><b>SOBOTA, 24&nbsp;sierpnia</b></td>
-            <td><b>Święto Św.&nbsp;Bartłomieja Apostoła</b></td>
+            <td><b>SOBOTA, 31&nbsp;sierpnia</b></td>
+            <td><b></b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>1. + Wiesława Napieracza<br>
-            2. Dzięk-błag. o&nbsp;Boże Błog., potrzebne łaski dla
-            Michała i&nbsp;Gabriela</td>
+            <td>+ Stanisławę Krakowiak</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td></td>
+            <td>1. + Lucjana Wodza, Wandę i&nbsp;Jana Stróżów<br>
+            2. + Edmunda Niewiadomskiego 8&nbsp;rocz. śm.</td>
         </tr>
         <tr class="actualnosci_day">
-            <td><b>NIEDZIELA, 25&nbsp;sierpnia</b></td>
+            <td><b>NIEDZIELA, 1&nbsp;września</b></td>
             <td><b>
-                <span style="color: red">XXI&nbsp;Niedziela Zwykła</span>
+                <span style="color: red">XXII&nbsp;Niedziela Zwykła</span>
             </b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">7.30</td>
-            <td>+ Zofię, Mariana Krawczyków, Józefa, Mariannę Mądrych</td>
+            <td>+ Anielę, Stefana i&nbsp;Jana Jędrzejewskich</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">9.30</td>
-            <td>+ Gustawa Polita – 8&nbsp;rocz. śm.</td>
+            <td>+ Teresę 7&nbsp;rocz. śm., Eugeniusza, zm.
+            z&nbsp;rodz. Paluchów</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">11.15</td>
-            <td>Za&nbsp;parafian</td>
+            <td>1. K.Ż.R. – Teresa Borkowska<br>
+            2. + Helenę, Jana, Bożenę, Ireneusza Kiliańskich, Sylwię
+            Pawłowską, zm. z&nbsp;rodz.</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">12.30</td>
-            <td>+ Stanisławę i&nbsp;Henryka Krakowiaków</td>
+            <td>1. Za&nbsp;parafian<br>
+            2. O&nbsp;Boże Błog., dary Ducha Świętego, opiekę MB dla
+            Hani w&nbsp;18&nbsp;rocz. urodzin</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>+ Roberta Mazura</td>
+            <td>+ Stanisławę, Jana, Stanisławę, Witolda, Mariana,
+            Krystyę i&nbsp;Mariana</td>
         </tr>
     </tbody>
 </table>
