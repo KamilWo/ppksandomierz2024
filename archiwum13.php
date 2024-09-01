fc0d22dd11da36117ee8e0a2155d900761d23ecf
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2024-09-01 18:10:51 +0100

    2024-09-01

--- archiwum13.php
+++ archiwum13.php
@@ -19,7 +19,135 @@ include 'head.php';
                     <!-- treść główna -->
                     <div class="col">
                         <h2 class="first-content-header">Archiwum wiadomości 2024 rok</h2>
-                        <h2>Okres 1.&nbsp;stycznia - 25.&nbsp;sierpnia 2024&nbsp;r.</h2>
+                        <h2>Okres 1.&nbsp;stycznia - 1.&nbsp;września 2024&nbsp;r.</h2>
+
+                        <div class="card">
+                            <div class="card-header">
+                                <h3>INTENCJE MSZY ŚWIĘTYCH<br>
+                                    25.08. – 1.09.2024&nbsp;r.
+                                </h3>
+                            </div>
+                            <div class="col">
+                        <table class="aktualnosci_table" cellpadding="5" cellspacing="0">
+                            <tbody>
+                                <tr class="actualnosci_day">
+                                    <td><b>PONIEDZIAŁEK, 26&nbsp;sierpnia</b></td>
+                                    <td><b>Uroczystość NMP Częstochowskiej</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>+ Edwarda Ziębę</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>Dziękczynna z&nbsp;racji 46&nbsp;rocz. ślubu:
+                                    Stanisławy i&nbsp;Stanisława Niezgodów</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>WTOREK, 27&nbsp;sierpnia</b></td>
+                                    <td><b>Wspomnienie św.&nbsp;Moniki</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. + Mariana Sapielaka<br>
+                                    2. + Henryka Libickiego</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>+ Adama Ozdobę</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>ŚRODA, 28&nbsp;sierpnia</b></td>
+                                    <td><b>Wspomnienie św.&nbsp;Augustyna, biskupa
+                                    i&nbsp;Doktora Kościoła</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. + Meczysława Kunkiewicza<br>
+                                    2. + Henryka Libickiego</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>+ Roberta Mazura</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>CZWARTEK, 29&nbsp;sierpnia</b></td>
+                                    <td><b>Wspomnenie Męczeństwa św.&nbsp;Jana
+                                    Chrzciciela</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>+ Edwarda Ziębę</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>+ Roberta Mazura</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>PIĄTEK, 30&nbsp;sierpnia</b></td>
+                                    <td><b></b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>+ Emilię, Szczepana Dudków, zm. z&nbsp;rodz., dusze
+                                    w&nbsp;czyśćcu cierpiące</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. + Kazimierza Karę<br>
+                                    2. + Jana, Klarę, Ryszarda Chrząstowskich, Jana
+                                    Makowskiego</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>SOBOTA, 31&nbsp;sierpnia</b></td>
+                                    <td><b></b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>+ Stanisławę Krakowiak</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. + Lucjana Wodza, Wandę i&nbsp;Jana Stróżów<br>
+                                    2. + Edmunda Niewiadomskiego 8&nbsp;rocz. śm.</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>NIEDZIELA, 1&nbsp;września</b></td>
+                                    <td><b>
+                                        <span style="color: red">XXII&nbsp;Niedziela Zwykła</span>
+                                    </b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">7.30</td>
+                                    <td>+ Anielę, Stefana i&nbsp;Jana Jędrzejewskich</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">9.30</td>
+                                    <td>+ Teresę 7&nbsp;rocz. śm., Eugeniusza, zm.
+                                    z&nbsp;rodz. Paluchów</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">11.15</td>
+                                    <td>1. K.Ż.R. – Teresa Borkowska<br>
+                                    2. + Helenę, Jana, Bożenę, Ireneusza Kiliańskich, Sylwię
+                                    Pawłowską, zm. z&nbsp;rodz.</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">12.30</td>
+                                    <td>1. Za&nbsp;parafian<br>
+                                    2. O&nbsp;Boże Błog., dary Ducha Świętego, opiekę MB dla
+                                    Hani w&nbsp;18&nbsp;rocz. urodzin</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>+ Stanisławę, Jana, Stanisławę, Witolda, Mariana,
+                                    Krystyę i&nbsp;Mariana</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                            </div>
+                        </div>
+                        <br>
 
                         <div class="card">
                             <div class="card-header">
