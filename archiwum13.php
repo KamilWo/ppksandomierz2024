252dc903b76f321ba30e8c9a1e7c758c24c6dc99
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2024-09-07 20:16:09 +0100

    2024-09-07

--- archiwum13.php
+++ archiwum13.php
@@ -19,7 +19,145 @@ include 'head.php';
                     <!-- treść główna -->
                     <div class="col">
                         <h2 class="first-content-header">Archiwum wiadomości 2024 rok</h2>
-                        <h2>Okres 1.&nbsp;stycznia - 1.&nbsp;września 2024&nbsp;r.</h2>
+                        <h2>Okres 1.&nbsp;stycznia - 8.&nbsp;września 2024&nbsp;r.</h2>
+
+                        <div class="card">
+                            <div class="card-header">
+                                <h3>INTENCJE MSZY ŚWIĘTYCH<br>
+                                    02.09. – 08.09.2024&nbsp;r.
+                                </h3>
+                            </div>
+                            <div class="col">
+                        <table class="aktualnosci_table" cellpadding="5" cellspacing="0">
+                            <tbody>
+                                <tr class="actualnosci_day">
+                                    <td><b>PONIEDZIAŁEK, 2&nbsp;września</b></td>
+                                    <td><b></b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>+ Annę Ziomek w&nbsp;1&nbsp;rocz. śm.,
+                                    zm.&nbsp;z&nbsp;rodz. Kędziorów i&nbsp;Maluszczaków</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">8.00</td>
+                                    <td>+ Halinę Hagdej 3&nbsp;rocz. śm.</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. + Marcina Huka<br>
+                                    2. + Stefanię i&nbsp;Jana Stąporowskich, Mariannę
+                                    i&nbsp;Stefana Kołeczków</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>WTOREK, 3&nbsp;września</b></td>
+                                    <td><b>Wspomnienie św.&nbsp;Grzegorza Wielkiego</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. Dzięk.-błag. o&nbsp;Boże Błog., opiekę MB dla Matki
+                                    i&nbsp;Syna<br>
+                                    2. + Teodorę Woźniak</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. + Bogdana Płodowskiego<br>
+                                    2. + Józefa, Reginę Buczków, Wandę, Stanisława, Antoniego
+                                    Słowików, Zofię, Mariana Wróblewskich</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>ŚRODA, 4&nbsp;września</b></td>
+                                    <td><b></b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. W int. dzieci i&nbsp;chrześniaków o&nbsp;łaskę
+                                    żywej wiary<br>
+                                    2. + Mariana i&nbsp;Zofię Miękinów</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. + Wojciecha Stolę<br>
+                                    2. + Wiesława Króla, Kazimierę Majchrowską</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>CZWARTEK, 5&nbsp;września</b></td>
+                                    <td><b>I&nbsp;Czwartek miesiąca</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. O&nbsp;świętość kapłanów i&nbsp;nowe powołania
+                                    kapłańskie<br>
+                                    2. + Krzysztofa w&nbsp;3&nbsp;rocz. śm., Wenronikę
+                                    w&nbsp;14&nbsp;rocz. śm. Paluchów</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. + Jacka Stolę<br>
+                                    2. + Joannę Zakrzewską w&nbsp;4&nbsp;rocz. śm.</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>PIĄTEK, 6&nbsp;września</b></td>
+                                    <td><b>I&nbsp;Piątek miesiąca</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. O&nbsp;łaskę dobrej śmierci<br>
+                                    2. + Mariana Miękinę</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. + Mariannę Antończyk<br>
+                                    2. + Roberta Mazura</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>SOBOTA, 7&nbsp;września</b></td>
+                                    <td><b>I&nbsp;Sobota miesiąca</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. + Mariana Sapielaka<br>
+                                    2. + Mariannę Banię</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. LM<br>
+                                    2. + Alicję Gilewską, Józefa, Stefanię Janusów, Teresę
+                                    Nowakowską, Zdzisława Gilewskiego</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>NIEDZIELA, 8&nbsp;września</b></td>
+                                    <td><b>
+                                        <span style="color: red">XXIII&nbsp;Niedziela Zwykła</span>
+                                    </b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">7.30</td>
+                                    <td>Dziękczynna z&nbsp;racji 86&nbsp;rocz. urodzin
+                                    Genowefy z&nbsp;prośbą o&nbsp;Boże Błog., opiekę MB</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">9.30</td>
+                                    <td>+ Stefanię 12&nbsp;rocz. śm., Kazimierza, Jana
+                                    Stanisława, Jerzego Słodowników</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">11.15</td>
+                                    <td>+ Stanisławę i&nbsp;Henryka Krakowiaków</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">12.30</td>
+                                    <td>+ Zofię Strzębską</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>Za&nbsp;parafian</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                            </div>
+                        </div>
+                        <br>
 
                         <div class="card">
                             <div class="card-header">
--- last_intentions.php
+++ last_intentions.php
@@ -1,134 +1,139 @@
 <div class="card">
     <div class="card-header">
         <h3>INTENCJE MSZY ŚWIĘTYCH<br>
-            02.09. – 08.09.2024&nbsp;r.
+            09.09. – 15.09.2024&nbsp;r.
         </h3>
     </div>
     <div class="col">
 <table class="aktualnosci_table" cellpadding="5" cellspacing="0">
     <tbody>
         <tr class="actualnosci_day">
-            <td><b>PONIEDZIAŁEK, 2&nbsp;września</b></td>
+            <td><b>PONIEDZIAŁEK, 9&nbsp;września</b></td>
             <td><b></b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>+ Annę Ziomek w&nbsp;1&nbsp;rocz. śm.,
-            zm.&nbsp;z&nbsp;rodz. Kędziorów i&nbsp;Maluszczaków</td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">8.00</td>
-            <td>+ Halinę Hagdej 3&nbsp;rocz. śm.</td>
+            <td>1. + Jana Radzimowskiego w&nbsp;8&nbsp;rocz. śm.<br>
+            2. + Helenę Anwajler</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>1. + Marcina Huka<br>
-            2. + Stefanię i&nbsp;Jana Stąporowskich, Mariannę
-            i&nbsp;Stefana Kołeczków</td>
+            <td>1. + Mariannę i&nbsp;Stefana Kwietniów, zm.
+            z&nbsp;rodz. Zybałów, Misiudów, Kwietniów
+            i&nbsp;Wiśniewskich<br>
+            2. + Zofię, Wacława Borcuchów, Michalinę i&nbsp;Franciszka
+            Irków</td>
         </tr>
         <tr class="actualnosci_day">
-            <td><b>WTOREK, 3&nbsp;września</b></td>
-            <td><b>Wspomnienie św.&nbsp;Grzegorza Wielkiego</b></td>
+            <td><b>WTOREK, 10&nbsp;września</b></td>
+            <td><b></b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>1. Dzięk.-błag. o&nbsp;Boże Błog., opiekę MB dla Matki
-            i&nbsp;Syna<br>
+            <td>1. + Bogdana Płodowskiego<br>
             2. + Teodorę Woźniak</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>1. + Bogdana Płodowskiego<br>
-            2. + Józefa, Reginę Buczków, Wandę, Stanisława, Antoniego
-            Słowików, Zofię, Mariana Wróblewskich</td>
+            <td>1. KWC<br>
+            2. O&nbsp;doświadczenie Bożej miłości i&nbsp;łaskę
+            nawrócenia dla męża Piotra, i&nbsp;ojca Tadeusza</td>
         </tr>
         <tr class="actualnosci_day">
-            <td><b>ŚRODA, 4&nbsp;września</b></td>
+            <td><b>ŚRODA, 11&nbsp;września</b></td>
             <td><b></b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>1. W int. dzieci i&nbsp;chrześniaków o&nbsp;łaskę
-            żywej wiary<br>
-            2. + Mariana i&nbsp;Zofię Miękinów</td>
+            <td>1. W&nbsp;int. Moniki i&nbsp;Lukasza o&nbsp;Boże
+            Błog., potrzebne łaski<br>
+            2. O&nbsp;Boże Błog. dla Joanny Skoczkowskiej</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>1. + Wojciecha Stolę<br>
-            2. + Wiesława Króla, Kazimierę Majchrowską</td>
+            <td>1. + Wandę, Michała, Urszulę, Marka Niezgodów<br>
+            2. + Joannę Branię</td>
         </tr>
         <tr class="actualnosci_day">
-            <td><b>CZWARTEK, 5&nbsp;września</b></td>
-            <td><b>I&nbsp;Czwartek miesiąca</b></td>
+            <td><b>CZWARTEK, 12&nbsp;września</b></td>
+            <td><b></b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>1. O&nbsp;świętość kapłanów i&nbsp;nowe powołania
-            kapłańskie<br>
-            2. + Krzysztofa w&nbsp;3&nbsp;rocz. śm., Wenronikę
-            w&nbsp;14&nbsp;rocz. śm. Paluchów</td>
+            <td>1. + Mariana Miękinę<br>
+            2. + Grażynę Stasiek</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>1. + Jacka Stolę<br>
-            2. + Joannę Zakrzewską w&nbsp;4&nbsp;rocz. śm.</td>
+            <td>1. + Weronikę Błaszczyk<br>
+            2. + Zdzisława Dusaka</td>
         </tr>
         <tr class="actualnosci_day">
-            <td><b>PIĄTEK, 6&nbsp;września</b></td>
-            <td><b>I&nbsp;Piątek miesiąca</b></td>
+            <td><b>PIĄTEK, 13&nbsp;września</b></td>
+            <td><b>Wspomnienie św.&nbsp;Jana Chryzostoma</b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>1. O&nbsp;łaskę dobrej śmierci<br>
-            2. + Mariana Miękinę</td>
+            <td>1. Wynagradzająca za&nbsp;grzechy eutanazji
+            i&nbsp;aborcji<br>
+            2. + Teodorę Woźniak</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>1. + Mariannę Antończyk<br>
+            <td>1. + Mariannę Pawłowską 25&nbsp;rocz. śm., Mikołaja
+            i&nbsp;Teodozję Załuskich<br>
             2. + Roberta Mazura</td>
         </tr>
         <tr class="actualnosci_day">
-            <td><b>SOBOTA, 7&nbsp;września</b></td>
-            <td><b>I&nbsp;Sobota miesiąca</b></td>
+            <td><b>SOBOTA, 14&nbsp;września</b></td>
+            <td><b>Święto Podwyższenia Krzyża Świętego<br>
+            Odpust parafialny</b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>1. + Mariana Sapielaka<br>
-            2. + Mariannę Banię</td>
+            <td>+ Bogdana Płodowskiego</td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">9.00</td>
+            <td>1. + Bogusława Sarzyńskiego, Annę, Janusza, Stanisława
+            Roźmiejów, Zofię, Mikołaja, Eugeniusza Kapustów<br>
+            2. + Marię, Władysława, Czesława Barańskich, Andrzeja
+            Krawczyka</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>1. LM<br>
-            2. + Alicję Gilewską, Józefa, Stefanię Janusów, Teresę
-            Nowakowską, Zdzisława Gilewskiego</td>
+            <td>1. + Janinę Lipiec<br>
+            2. Za&nbsp;parafian</td>
         </tr>
         <tr class="actualnosci_day">
-            <td><b>NIEDZIELA, 8&nbsp;września</b></td>
+            <td><b>NIEDZIELA, 15&nbsp;września</b></td>
             <td><b>
-                <span style="color: red">XXIII&nbsp;Niedziela Zwykła</span>
+                <span style="color: red">XXIV&nbsp;Niedziela Zwykła</span>
             </b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">7.30</td>
-            <td>Dziękczynna z&nbsp;racji 86&nbsp;rocz. urodzin
-            Genowefy z&nbsp;prośbą o&nbsp;Boże Błog., opiekę MB</td>
+            <td>+ Mariannę Niziałek 20&nbsp;rocz. śm., Wacława
+            Niziałka</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">9.30</td>
-            <td>+ Stefanię 12&nbsp;rocz. śm., Kazimierza, Jana
-            Stanisława, Jerzego Słodowników</td>
+            <td>+ Jerzego Janiszewskiego – miesiąc
+            po&nbsp;śmierci</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">11.15</td>
-            <td>+ Stanisławę i&nbsp;Henryka Krakowiaków</td>
+            <td>W&nbsp;int. Domowego Kościoła</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">12.30</td>
-            <td>+ Zofię Strzębską</td>
+            <td>+ Anetę Ryzińską w&nbsp;rocz. śm.</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>Za&nbsp;parafian</td>
+            <td>Dzięk.-błag. o&nbsp;Miłosierdzie Boże, wiarę, nadzieję
+            i&nbsp;miłość, łaskę pokory, nawrócenie i&nbsp;uzdrowienie
+            w&nbsp;rodzinie</td>
         </tr>
     </tbody>
 </table>
