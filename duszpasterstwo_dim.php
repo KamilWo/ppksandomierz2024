96bfe81c0bfd697e4ef61ccd877ce0e75a4c3c4f
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2024-09-08 10:49:54 +0100

    2024-09-08

--- duszpasterstwo_dim.php
+++ duszpasterstwo_dim.php
@@ -1,11 +1,45 @@
-<h2 class="first-content-header"><i class="fa fa-users-line"></i> Duszpasterstwo dzieci i&nbsp;młodzieży</h2>
+<h2 class="first-content-header">
+    <i class="fa fa-users-line"></i> Duszpasterstwo dzieci i&nbsp;młodzieży
+</h2>
 
 <div class="card">
     <div class="card-header">
-        Zmiany w&nbsp;tej części już wkrótce
+        Spotkania w&nbsp;Duszpasterstwie Dzieci i&nbsp;Młodzieży
     </div>
     <div class="col">
-        <p>Sekcja strony w&nbsp;przygotowaniu</p>
+        <p>
+            <strong>Zapraszamy na&nbsp;spotkania duszpasterskie dla
+            dzieci i&nbsp;młodzieży przygotowujących się do&nbsp;przyjęcia
+            Sakramentu Bierzmowania, Pierwszej Komunii świętej, oraz
+            przeżywających rocznicę Komunii świętej w&nbsp;naszej
+            parafii.</strong>
+        </p>
+        <ol>
+            <li>
+                <strong>Dzieci pierwszokomunijne</strong> z&nbsp;klas
+                trzecich z&nbsp;rodzicami oraz <strong>dzieci
+                rocznicowe</strong> z&nbsp;klas
+                czwartych z&nbsp;rodzicami zapraszamy na&nbsp;obowiązkowe
+                spotkanie w&nbsp;<strong>niedzielę 15&nbsp;września</strong>.
+                O&nbsp;godzinie 12:30 Msza święta z&nbsp;kazaniem dla dzieci,
+                a&nbsp;po&nbsp;Mszy świętej spotkanie organizacyjne
+                w&nbsp;kościele.
+            </li>
+            <li>
+                <strong>Kandydatów do&nbsp;bierzmowania</strong>
+                z&nbsp;klas: 6, 7 i&nbsp;8&nbsp;oraz ich rodziców zapraszamy
+                na&nbsp;obowiązkowe spotkanie we&nbsp;wspomnienie Świętego
+                Stanisława Kostki, patrona młodzieży we&nbsp;<strong>środę
+                18&nbsp;września</strong>. O&nbsp;godzinie 18:00 Msza święta
+                i&nbsp;po&nbsp;Mszy świętej spotkanie w&nbsp;kościele.
+                Pragniemy przypomnieć, że&nbsp;według „Wskazań Episkopatu
+                Polski dotyczących przygotowania do&nbsp;sakramentu
+                bierzmowania”, w&nbsp;naszej parafii przygotowanie młodzieży
+                do&nbsp;sakramentu bierzmowania odbywa się przez 3&nbsp;lata
+                (w&nbsp;6, 7 i&nbsp;8&nbsp;klasie szkoły podstawowej).
+                Sakrament ten jest udzielany przy końcu 8&nbsp;klasy.
+            </li>
+        </ol>
     </div>
 </div>
 <br>
