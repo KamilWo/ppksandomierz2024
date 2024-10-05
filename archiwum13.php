ca7a3335855439d840bf669fc1d565d77c325040
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2024-10-05 19:25:43 +0100

    2024-10-06

--- archiwum13.php
+++ archiwum13.php
@@ -674,6 +674,48 @@ include 'head.php';
 
                         <div class="card">
                             <div class="card-header">
+                                Spotkania Duszpasterstwa Dzieci i&nbsp;Młodzieży
+                            </div>
+                            <div class="col">
+                                <p>
+                                    <strong>Zapraszamy na&nbsp;spotkania duszpasterskie dla
+                                    dzieci i&nbsp;młodzieży przygotowujących się do&nbsp;przyjęcia
+                                    Sakramentu Bierzmowania, Pierwszej Komunii świętej, oraz
+                                    przeżywających rocznicę Komunii świętej w&nbsp;naszej
+                                    parafii.</strong>
+                                </p>
+                                <ol>
+                                    <li>
+                                        <strong>Dzieci pierwszokomunijne</strong> z&nbsp;klas
+                                        trzecich z&nbsp;rodzicami oraz <strong>dzieci
+                                        rocznicowe</strong> z&nbsp;klas
+                                        czwartych z&nbsp;rodzicami zapraszamy na&nbsp;obowiązkowe
+                                        spotkanie w&nbsp;<strong>niedzielę 15&nbsp;września</strong>.
+                                        O&nbsp;godzinie 12:30 Msza święta z&nbsp;kazaniem dla dzieci,
+                                        a&nbsp;po&nbsp;Mszy świętej spotkanie organizacyjne
+                                        w&nbsp;kościele.
+                                    </li>
+                                    <li>
+                                        <strong>Kandydatów do&nbsp;bierzmowania</strong>
+                                        z&nbsp;klas: 6, 7 i&nbsp;8&nbsp;oraz ich rodziców zapraszamy
+                                        na&nbsp;obowiązkowe spotkanie we&nbsp;wspomnienie Świętego
+                                        Stanisława Kostki, patrona młodzieży we&nbsp;<strong>środę
+                                        18&nbsp;września</strong>. O&nbsp;godzinie 18:00 Msza święta
+                                        i&nbsp;po&nbsp;Mszy świętej spotkanie w&nbsp;kościele.
+                                        Pragniemy przypomnieć, że&nbsp;według „Wskazań Episkopatu
+                                        Polski dotyczących przygotowania do&nbsp;sakramentu
+                                        bierzmowania”, w&nbsp;naszej parafii przygotowanie młodzieży
+                                        do&nbsp;sakramentu bierzmowania odbywa się przez 3&nbsp;lata
+                                        (w&nbsp;6, 7 i&nbsp;8&nbsp;klasie szkoły podstawowej).
+                                        Sakrament ten jest udzielany przy końcu 8&nbsp;klasy.
+                                    </li>
+                                </ol>
+                            </div>
+                        </div>
+                        <br>
+
+                        <div class="card">
+                            <div class="card-header">
                                 <h3>INTENCJE MSZY ŚWIĘTYCH<br>
                                     25.08. – 1.09.2024&nbsp;r.
                                 </h3>
--- duszpasterstwo_dim.php
+++ duszpasterstwo_dim.php
@@ -4,40 +4,45 @@
 
 <div class="card">
     <div class="card-header">
-        Spotkania w&nbsp;Duszpasterstwie Dzieci i&nbsp;Młodzieży
+        <h3>Spotkania Duszpasterstwa Dzieci i&nbsp;Młodzieży<br>
+        w&nbsp;parafii Podwyższenia Krzyża Świętego w&nbsp;Sandomierzu</h3>
     </div>
     <div class="col">
-        <p>
+        <h4>
             <strong>Zapraszamy na&nbsp;spotkania duszpasterskie dla
             dzieci i&nbsp;młodzieży przygotowujących się do&nbsp;przyjęcia
             Sakramentu Bierzmowania, Pierwszej Komunii świętej, oraz
             przeżywających rocznicę Komunii świętej w&nbsp;naszej
             parafii.</strong>
-        </p>
+        </h4>
+        <br>
         <ol>
             <li>
-                <strong>Dzieci pierwszokomunijne</strong> z&nbsp;klas
-                trzecich z&nbsp;rodzicami oraz <strong>dzieci
-                rocznicowe</strong> z&nbsp;klas
-                czwartych z&nbsp;rodzicami zapraszamy na&nbsp;obowiązkowe
-                spotkanie w&nbsp;<strong>niedzielę 15&nbsp;września</strong>.
-                O&nbsp;godzinie 12:30 Msza święta z&nbsp;kazaniem dla dzieci,
-                a&nbsp;po&nbsp;Mszy świętej spotkanie organizacyjne
-                w&nbsp;kościele.
+                <h5>KANDYDACI NA MINISTRANTA I MŁODSI MINISTRANCI</h5>
+                <p>Spotkanie w kościele 12 października o godz. 11:30</p>
             </li>
+
             <li>
-                <strong>Kandydatów do&nbsp;bierzmowania</strong>
-                z&nbsp;klas: 6, 7 i&nbsp;8&nbsp;oraz ich rodziców zapraszamy
-                na&nbsp;obowiązkowe spotkanie we&nbsp;wspomnienie Świętego
-                Stanisława Kostki, patrona młodzieży we&nbsp;<strong>środę
-                18&nbsp;września</strong>. O&nbsp;godzinie 18:00 Msza święta
-                i&nbsp;po&nbsp;Mszy świętej spotkanie w&nbsp;kościele.
-                Pragniemy przypomnieć, że&nbsp;według „Wskazań Episkopatu
-                Polski dotyczących przygotowania do&nbsp;sakramentu
-                bierzmowania”, w&nbsp;naszej parafii przygotowanie młodzieży
-                do&nbsp;sakramentu bierzmowania odbywa się przez 3&nbsp;lata
-                (w&nbsp;6, 7 i&nbsp;8&nbsp;klasie szkoły podstawowej).
-                Sakrament ten jest udzielany przy końcu 8&nbsp;klasy.
+                <h5>GRUPA MŁODZIEŻOWA</h5>
+                <p>Spotkanie w salce parafialnej 18 października o godz. 18:30</p>
+            </li>
+
+            <li>
+                <h5>DZIECI PIERWSZOKOMUNIJNE I ROCZNICOWE</h5>
+                <p>Spotkanie w kościele 26 października o godz. 10:00</p>
+            </li>
+
+            <li>
+                <h5>KANDYDACI DO BIERZMOWANIA (klasy 6, 7 i 8)</h5>
+                <p>Spotkanie w kościele 27 października o godz. 12:30</p>
+            </li>
+
+            <li>
+                <h5>ZAJĘCIA SPORTOWE NA HALI (dla szkoły podstawowej)</h5>
+                <p>Hala sportowa przy ulicy Cieśli w Sandomierzu</p>
+                <p>12 października od 12:15 do 14:15</p>
+                <p>19 października od 14 do 16</p>
+                <p>26 października od 14 do 16</p>
             </li>
         </ol>
     </div>
