b1ee1c5ed4fa488618113fe222fbf21c230941f5
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2024-10-05 19:21:00 +0100

    2024-10-06

--- archiwum13.php
+++ archiwum13.php
@@ -19,7 +19,140 @@ include 'head.php';
                     <!-- treść główna -->
                     <div class="col">
                         <h2 class="first-content-header">Archiwum wiadomości 2024 rok</h2>
-                        <h2>Okres 1.&nbsp;stycznia - 15.&nbsp;września 2024&nbsp;r.</h2>
+                        <h2>Okres 1.&nbsp;stycznia - 29.&nbsp;września 2024&nbsp;r.</h2>
+
+                        <div class="card">
+                            <div class="card-header">
+                                <h3>INTENCJE MSZY ŚWIĘTYCH<br>
+                                    23.09. – 29.09.2024&nbsp;r.
+                                </h3>
+                            </div>
+                            <div class="col">
+                        <table class="aktualnosci_table" cellpadding="5" cellspacing="0">
+                            <tbody>
+                                <tr class="actualnosci_day">
+                                    <td><b>PONIEDZIAŁEK, 23&nbsp;września</b></td>
+                                    <td><b>Wspomnienie Św.&nbsp;Pio
+                                    z&nbsp;Pietrelciny</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. + Jacentego Dudka<br>
+                                    2. + Helenę Anwajler</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. + Roberta Burka<br>
+                                    2. + Natalię Warzochę</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>WTOREK, 24&nbsp;września</b></td>
+                                    <td><b></b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. + Mariana Sapielaka<br>
+                                    2. + Mariana Miękinę<br>
+                                    3. O&nbsp;Boże Błog., potrzebne łaski dla Anny
+                                    i&nbsp;Witolda w&nbsp;13&nbsp;rocz. ślubu</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. + Reginę Jońca</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>ŚRODA, 25&nbsp;września</b></td>
+                                    <td><b></b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. + Jacka Ziółko<br>
+                                    2. + Jacka Wosia</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. + Helenę Anwajler<br>
+                                    2. + Reginę Jońca<br>
+                                    3. + Czesława Dudę</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>CZWARTEK, 26&nbsp;września</b></td>
+                                    <td><b></b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. + Mariana Sapielaka<br>
+                                    2. + Jacka Wosia</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. + Jadwigę, Bolesława, Helenę, Józefa Rutkowskich,
+                                    Antoniego, Stanisława, Józefę Marców, Wiesława, Barbarę
+                                    Sowiarów, Bogusława Bieńka<br>
+                                    2. + Roberta Burka</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>PIĄTEK, 27&nbsp;września</b></td>
+                                    <td><b>Wspomnienie Św.&nbsp;Wincentego
+                                    a&nbsp;Paulo</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. + Roberta Burka<br>
+                                    2. + Mariana Miękinę</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. + Reginę Jońca<br>
+                                    2. + Józefa Wasińskiego</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>SOBOTA, 28&nbsp;września</b></td>
+                                    <td><b>Wspomnienie Św.&nbsp;Wacława</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. + Franciszka Turka<br>
+                                    2. + Aleksandrę Szcześniak</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. + Jacentego Dudka<br>
+                                    2. + Natalię Warzochę</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>NIEDZIELA, 29&nbsp;września</b></td>
+                                    <td><b>
+                                        <span style="color: red">XXVI&nbsp;Niedziela Zwykła</span>
+                                    </b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">7.30</td>
+                                    <td>+ Jacentego Dudka</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">9.30</td>
+                                    <td>+ Reginę Jońca</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">11.15</td>
+                                    <td>Za&nbsp;parafian</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">12.30</td>
+                                    <td>+ Macieja Wodza, Bożenę Kubalę – 2&nbsp;rocz. śm.,
+                                    Jana, Florentynę, Mieczysława Sado, Stefana, Krystynę
+                                    Wodzów</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>+ Reginę Jońca</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                            </div>
+                        </div>
+                        <br>
 
                         <div class="card">
                             <div class="card-header">
--- last_intentions.php
+++ last_intentions.php
@@ -1,129 +1,136 @@
 <div class="card">
     <div class="card-header">
         <h3>INTENCJE MSZY ŚWIĘTYCH<br>
-            23.09. – 29.09.2024&nbsp;r.
+            7.10. – 13.10.2024&nbsp;r.
         </h3>
     </div>
     <div class="col">
 <table class="aktualnosci_table" cellpadding="5" cellspacing="0">
     <tbody>
         <tr class="actualnosci_day">
-            <td><b>PONIEDZIAŁEK, 23&nbsp;września</b></td>
-            <td><b>Wspomnienie Św.&nbsp;Pio
-            z&nbsp;Pietrelciny</b></td>
+            <td><b>PONIEDZIAŁEK, 7&nbsp;października</b></td>
+            <td><b>Wspomnienie NMP Różańcowej</b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>1. + Jacentego Dudka<br>
+            <td>1. + Jana, Józefę Marców, Ryszarda Pietruszkę, zm.
+                z&nbsp;rodz. Marców<br>
             2. + Helenę Anwajler</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>1. + Roberta Burka<br>
-            2. + Natalię Warzochę</td>
+            <td>1. + Krzysztofa Stępnia 12&nbsp;rocz. śm.<br>
+            2. + Władysławę i&nbsp;Jana Nizielskich, Stanisławę
+            Muchę</td>
         </tr>
         <tr class="actualnosci_day">
-            <td><b>WTOREK, 24&nbsp;września</b></td>
+            <td><b>WTOREK, 8&nbsp;października</b></td>
             <td><b></b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>1. + Mariana Sapielaka<br>
-            2. + Mariana Miękinę<br>
-            3. O&nbsp;Boże Błog., potrzebne łaski dla Anny
-            i&nbsp;Witolda w&nbsp;13&nbsp;rocz. ślubu</td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">18.00</td>
-            <td>1. + Reginę Jońca</td>
-        </tr>
-        <tr class="actualnosci_day">
-            <td><b>ŚRODA, 25&nbsp;września</b></td>
-            <td><b></b></td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">6.30</td>
-            <td>1. + Jacka Ziółko<br>
+            <td>1. + Marię i&nbsp;Jana Politów<br>
             2. + Jacka Wosia</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>1. + Helenę Anwajler<br>
-            2. + Reginę Jońca<br>
-            3. + Czesława Dudę</td>
+            <td>1. W&nbsp;intencji osób uzależnionych i&nbsp;ich
+                rodzin o&nbsp;uwolnienie z&nbsp;nałogu – KWC<br>
+            2. + Stanisława Kabzę 7&nbsp;rocz. śm.</td>
         </tr>
         <tr class="actualnosci_day">
-            <td><b>CZWARTEK, 26&nbsp;września</b></td>
-            <td><b></b></td>
+            <td><b>ŚRODA, 9&nbsp;października</b></td>
+            <td><b><span style="color: red">Uroczystość
+            bł.&nbsp;Wincentego Kadłubka, Patrona
+            Diecezji</span></b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>1. + Mariana Sapielaka<br>
-            2. + Jacka Wosia</td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">18.00</td>
-            <td>1. + Jadwigę, Bolesława, Helenę, Józefa Rutkowskich,
-            Antoniego, Stanisława, Józefę Marców, Wiesława, Barbarę
-            Sowiarów, Bogusława Bieńka<br>
-            2. + Roberta Burka</td>
-        </tr>
-        <tr class="actualnosci_day">
-            <td><b>PIĄTEK, 27&nbsp;września</b></td>
-            <td><b>Wspomnienie Św.&nbsp;Wincentego
-            a&nbsp;Paulo</b></td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">6.30</td>
-            <td>1. + Roberta Burka<br>
-            2. + Mariana Miękine</td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">18.00</td>
             <td>1. + Reginę Jońca<br>
-            2. + Józefa Wasińskiego</td>
+            2. + Czesława Godlewskiego</td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>1. + Tadeusza w&nbsp;rocz. śm., Irenę, Krzysztofa,
+                Mirosława Kołaczów<br>
+            2. + Jacentego Dudka</td>
         </tr>
         <tr class="actualnosci_day">
-            <td><b>SOBOTA, 28&nbsp;września</b></td>
-            <td><b>Wspomnienie Św.&nbsp;Wacława</b></td>
+            <td><b>CZWARTEK, 10&nbsp;października</b></td>
+            <td><b></b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>1. + Franciszka Turka<br>
+            <td>1. + Stefanię rocz. śm., Franciszka Kowalskich, Marię
+                Woś, Wiesławę Wałcerz<br>
+            2. + Janusza Nawrota i&nbsp;jego zm. rodziców, zm.
+            z&nbsp;rodz. Syryjczyków</td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>1. + Aleksandra, Tomasza, Michała Krakowiaków<br>
+            2. O&nbsp;doświadczenie Bożej miłości i&nbsp;łaskę
+            nawrócenia dla męża Piotra, i&nbsp;ojca Tadeusza</td>
+        </tr>
+        <tr class="actualnosci_day">
+            <td><b>PIĄTEK, 11&nbsp;października</b></td>
+            <td><b></b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>1. + Czesława Dudę<br>
             2. + Aleksandrę Szcześniak</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>1. + Jacentego Dudka<br>
+            <td>1. + Jacka Ziółka<br>
+            2. + Reginę Jońca</td>
+        </tr>
+        <tr class="actualnosci_day">
+            <td><b>SOBOTA, 12&nbsp;października</b></td>
+            <td><b></b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>1. + Kazimierza Płazę, zm. z&nbsp;rodz. i&nbsp;dusze
+                w&nbsp;czyśćcu cierpiące<br>
+            2. + Aleksandrę Durę</td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>1. + Helenę Król, zm. z&nbsp;rodz. Królów,
+                Ostrowskich, Szczębarów, Golców, Bednarskich
+                i&nbsp;dusze w&nbsp;czyśćcu cierpiące<br>
             2. + Natalię Warzochę</td>
         </tr>
         <tr class="actualnosci_day">
-            <td><b>NIEDZIELA, 29&nbsp;września</b></td>
+            <td><b>NIEDZIELA, 13&nbsp;października</b></td>
             <td><b>
-                <span style="color: red">XXVI&nbsp;Niedziela Zwykła</span>
+                <span style="color: red">XXVIII&nbsp;Niedziela Zwykła</span>
             </b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">7.30</td>
-            <td>+ Jacentego Dudka</td>
+            <td>+ Krystynę i&nbsp;Tadeusza Karasińskich</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">9.30</td>
-            <td>+ Reginę Jońca</td>
+            <td>+ Józefa, Karolinę, Jana Miszczaków, Antoniego
+            Szeligę</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">11.15</td>
-            <td>Za&nbsp;parafian</td>
+            <td>+ Otolię i&nbsp;Czesława Ciach</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">12.30</td>
-            <td>+ Macieja Wodza, Bożenę Kubalę – 2&nbsp;rocz. śm.,
-            Jana, Florentynę, Mieczysława Sado, Stefana, Krystynę
-            Wodzów</td>
+            <td>W&nbsp;int. Wioletty i&nbsp;Mirosława o&nbsp;Boże
+            błog., potrzebne łaski i&nbsp;dary Ducha Świętego
+            z&nbsp;okazji 18&nbsp;rocz. ślubu</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>+ Reginę Jońca</td>
+            <td>Za&nbsp;parafian</td>
         </tr>
     </tbody>
 </table>
