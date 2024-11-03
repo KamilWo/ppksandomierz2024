f858069c87341876ccde18784339ebf9bd71aa9a
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2024-11-03 10:47:39 +0000

    2024-11-03

--- archiwum13.php
+++ archiwum13.php
@@ -19,7 +19,158 @@ include 'head.php';
                     <!-- treść główna -->
                     <div class="col">
                         <h2 class="first-content-header">Archiwum wiadomości 2024 rok</h2>
-                        <h2>Okres 1.&nbsp;stycznia - 27.&nbsp;października 2024&nbsp;r.</h2>
+                        <h2>Okres 1.&nbsp;stycznia - 3.&nbsp;listopada 2024&nbsp;r.</h2>
+
+                        <div class="card">
+                            <div class="card-header">
+                                <h3>INTENCJE MSZY ŚWIĘTYCH<br>
+                                    28.10. – 3.11.2024&nbsp;r.
+                                </h3>
+                            </div>
+                            <div class="col">
+                        <table class="aktualnosci_table" cellpadding="5" cellspacing="0">
+                            <tbody>
+                                <tr class="actualnosci_day">
+                                    <td><b>PONIEDZIAŁEK, 28&nbsp;października</b></td>
+                                    <td><b>Święto Świętych Apostołów Szymona i&nbsp;Judy
+                                    Tadeusza</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. + Wacława, Stanisława, Karolinę, Stanisława Kmiecików<br>
+                                    2. + Helenę Anwajler</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. + Józefa Wasińskiego<br>
+                                    2. + Leszka Bobolę</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>WTOREK, 29&nbsp;października</b></td>
+                                    <td><b></b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. + Franciszka Turka<br>
+                                        2. + Henryka Libickiego<br>
+                                    3. + Wiesława Napieracza 1&nbsp;rocz. śm., Halinę
+                                    i&nbsp;Wacława Bryłów</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. + Jana Wnuka 23&nbsp;rocz. śm.<br>
+                                    2. + Jacentego Dudka</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>ŚRODA, 30&nbsp;października</b></td>
+                                    <td><b></b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. + Franciszka Turka<br>
+                                    2. + Tadeusza Piechowskiego</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. + Stanisławę Krakowiak<br>
+                                    2. + Bartłomieja Wrzoska 31&nbsp;rocz. śm., Ewę, Jana,
+                                    Teresę i&nbsp;Janinę</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>CZWARTEK, 31&nbsp;października</b></td>
+                                    <td><b></b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. + Leszka Bobolę<br>
+                                    2. + Tadeusza Piechowskiego</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. + Stanisława, Jana, Stanisława, Witolda, Mariana,
+                                        Krystynę, Mariana i&nbsp;Janinę<br>
+                                    2. + z&nbsp;rodz. Chorabiów, Andrzeja i&nbsp;Wiesława
+                                    Wołczyńskich, Krystynę Wziątek<br>
+                                    3. + Tadeusza Krupę</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>PIĄTEK, 1&nbsp;listopada</b></td>
+                                    <td><b><span style="color: red">Uroczystość Wszystkich
+                                    Świętych</span><br>
+                                    I&nbsp;piątek miesiąca</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">7.30</td>
+                                    <td>1. + Mariannę, Stanisława Pyszczyńskich; Barbarę
+                                        Puszczyńską<br>
+                                    2. + Stanisława Libickiego</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">9.30</td>
+                                    <td>1. + Wiktorię, Władysława Politów, Helenę
+                                    i&nbsp;Władysława Sudołów</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">11.15</td>
+                                    <td>1. + Marka Stefaniaka 20 rocz. śm.</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>Za&nbsp;parafian</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>SOBOTA, 2&nbsp;listopada</b></td>
+                                    <td><b>Wspomnienie Wszystkich Wiernych Zmarłych<br>
+                                    I&nbsp;sobota miesiąca</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. + z&nbsp;rodz. Dudków i&nbsp;Gorajów<br>
+                                    2. + Aleksandrę Szcześniak</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">9.00</td>
+                                    <td>Za&nbsp;zmarłych parafian</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. LM<br>
+                                    2. + Tadeusza Osucha w&nbsp;rocz. śm.</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>NIEDZIELA, 3&nbsp;listopada</b></td>
+                                    <td><b>
+                                        <span style="color: red">XXXI&nbsp;Niedziela Zwykła</span>
+                                    </b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">7.30</td>
+                                    <td>+ z&nbsp;rodz. Witek i&nbsp;Podsiadły</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">9.30</td>
+                                    <td>+ Stanisława, Leokadię, Romana, Marię Majchrów,
+                                    Krzysztofa, Stefanię, Józefa Łukawskich, Jadwigę, Zofię,
+                                    Stanisława Karasińskich</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">11.15</td>
+                                    <td>K.Ż.R. – od&nbsp;Róży z&nbsp;Milczan</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">12.30</td>
+                                    <td>Za&nbsp;parafian</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>+ Mariana Klimonta, Aleksandrę Ciamagę i&nbsp;zm.
+                                    z&nbsp;rodz.</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                            </div>
+                        </div>
+                        <br>
 
                         <div class="card">
                             <div class="card-header">
--- last_intentions.php
+++ last_intentions.php
@@ -1,147 +1,135 @@
 <div class="card">
     <div class="card-header">
         <h3>INTENCJE MSZY ŚWIĘTYCH<br>
-            28.10. – 3.11.2024&nbsp;r.
+            4.11. – 10.11.2024&nbsp;r.
         </h3>
     </div>
     <div class="col">
 <table class="aktualnosci_table" cellpadding="5" cellspacing="0">
     <tbody>
         <tr class="actualnosci_day">
-            <td><b>PONIEDZIAŁEK, 28&nbsp;października</b></td>
-            <td><b>Święto Świętych Apostołów Szymona i&nbsp;Judy
-            Tadeusza</b></td>
+            <td><b>PONIEDZIAŁEK, 4&nbsp;listopada</b></td>
+            <td><b>Wspomnienie Św.&nbsp;Karola Boromeusza</b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>1. + Wacława, Stanisława, Karolinę, Stanisława Kmiecików<br>
+            <td>1. Dzięk-błag. o&nbsp;zdrowie, Boże błog., dary Ducha
+                Świętego i&nbsp;opiekę MB dla Marcina<br>
+            2. + Henryka Libickiego<br>
+            3. + Waldemara Pałacha</td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>1. + Tomasza Chlebnego, Anielę i&nbsp;Antoniego
+                Gospodarczyków<br>
+            2. + Jana Szczębarę, zm. z&nbsp;rodz. Szczębarów, Golców,
+            Królów, Ostrowskich, Bednarskich i&nbsp;dusze
+            w&nbsp;czyśćcu cierpiące</td>
+        </tr>
+        <tr class="actualnosci_day">
+            <td><b>WTOREK, 5&nbsp;listopada</b></td>
+            <td><b></b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>1. + z&nbsp;rodziny i&nbsp;dusze w&nbsp;czyśćcu
+                cierpiące<br>
+            2. + Tadeusza Piechowskiego<br>
+            3. + Mariana Strzębskiego – m-c po&nbsp;śmierci</td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>1. + Mariannę i&nbsp;Eugeniusza Jedlińskich<br>
+            2. + Dariusza Chmiela</td>
+        </tr>
+        <tr class="actualnosci_day">
+            <td><b>ŚRODA, 6&nbsp;listopada</b></td>
+            <td><b></b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>1. + z rodz. Kasprzyckich i&nbsp;Nowaków<br>
+            2. + Aleksandrę Durę</td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>1. + z&nbsp;Legionu Maryi<br>
+            2. + Janinę rocz. śm., Stefana Plachów, Kazimierę, Edmunda
+            Pietrasów, Genowefę, Stefana Burków, Mariannę
+            i&nbsp;Zdzisława Potockich</td>
+        </tr>
+        <tr class="actualnosci_day">
+            <td><b>CZWARTEK, 7&nbsp;listopada</b></td>
+            <td><b>I&nbsp;Czwartek miesiąca</b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>1. O&nbsp;świętość kapłanów i&nbsp;nowe powołania
+                kapłańskie<br>
             2. + Helenę Anwajler</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>1. + Józefa Wasińskiego<br>
-            2. + Leszka Bobolę</td>
-        </tr>
-        <tr class="actualnosci_day">
-            <td><b>WTOREK, 29&nbsp;października</b></td>
-            <td><b></b></td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">6.30</td>
-            <td>1. + Franciszka Turka<br>
-                2. + Henryka Libickiego<br>
-            3. + Wiesława Napieracza 1&nbsp;rocz. śm., Halinę
-            i&nbsp;Wacława Bryłów</td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">18.00</td>
-            <td>1. + Jana Wnuka 23&nbsp;rocz. śm.<br>
+            <td>1. + Katarzynę Zielińską<br>
             2. + Jacentego Dudka</td>
         </tr>
         <tr class="actualnosci_day">
-            <td><b>ŚRODA, 30&nbsp;października</b></td>
+            <td><b>PIĄTEK, 8&nbsp;listopada</b></td>
             <td><b></b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>1. + Franciszka Turka<br>
-            2. + Tadeusza Piechowskiego</td>
+            <td>1. + z&nbsp;rodz. Mądrych i&nbsp;Steców<br>
+            2. + Helenę Anwajler</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>1. + Stanisławę Krakowiak<br>
-            2. + Bartłomieja Wrzoska 31&nbsp;rocz. śm., Ewę, Jana,
-            Teresę i&nbsp;Janinę</td>
+            <td>1. + Jana Mikulskiego<br>
+            2. + Janinę, Ludwika, Zofię, Ludwika Wilków, Teresę
+            i&nbsp;Jerzego Głowackich<br>
+            3. + Reginę Jońca</td>
         </tr>
         <tr class="actualnosci_day">
-            <td><b>CZWARTEK, 31&nbsp;października</b></td>
-            <td><b></b></td>
+            <td><b>SOBOTA, 9&nbsp;listopada</b></td>
+            <td><b>Święto Rocznicy Poświęcenia Bazyliki Lateraneńskiej</b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>1. + Leszka Bobolę<br>
-            2. + Tadeusza Piechowskiego</td>
+            <td>1. + z&nbsp;rodz. Kochańczyków i&nbsp;Kubików<br>
+            2. + Henryka Libickiego</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>1. + Stanisława, Jana, Stanisława, Witolda, Mariana,
-                Krystynę, Mariana i&nbsp;Janinę<br>
-            2. + z&nbsp;rodz. Chorabiów, Andrzeja i&nbsp;Wiesława
-            Wołczyńskich, Krystynę Wziątek<br>
-            3. + Tadeusza Krupę</td>
+            <td>1. + Roberta Mazura 1&nbsp;rocz. śm.<br>
+            2. + Jacentego Dudka</td>
         </tr>
         <tr class="actualnosci_day">
-            <td><b>PIĄTEK, 1&nbsp;listopada</b></td>
-            <td><b><span style="color: red">Uroczystość Wszystkich
-            Świętych</span><br>
-            I&nbsp;piątek miesiąca</b></td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">7.30</td>
-            <td>1. + Mariannę, Stanisława Pyszczyńskich; Barbarę
-                Puszczyńską<br>
-            2. + Stanisława Libickiego</td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">9.30</td>
-            <td>1. + Wiktorię, Władysława Politów, Helenę
-            i&nbsp;Władysława Sudołów</td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">11.15</td>
-            <td>1. + Marka Stefaniaka 20 rocz. śm.</td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">18.00</td>
-            <td>Za&nbsp;parafian</td>
-        </tr>
-        <tr class="actualnosci_day">
-            <td><b>SOBOTA, 2&nbsp;listopada</b></td>
-            <td><b>Wspomnienie Wszystkich Wiernych Zmarłych<br>
-            I&nbsp;sobota miesiąca</b></td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">6.30</td>
-            <td>1. + z&nbsp;rodz. Dudków i&nbsp;Gorajów<br>
-            2. + Aleksandrę Szcześniak</td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">9.00</td>
-            <td>Za&nbsp;zmarłych parafian</td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">18.00</td>
-            <td>1. LM<br>
-            2. + Tadeusza Osucha w&nbsp;rocz. śm.</td>
-        </tr>
-        <tr class="actualnosci_day">
-            <td><b>NIEDZIELA, 3&nbsp;listopada</b></td>
+            <td><b>NIEDZIELA, 10&nbsp;listopada</b></td>
             <td><b>
-                <span style="color: red">XXXI&nbsp;Niedziela Zwykła</span>
+                <span style="color: red">XXXII&nbsp;Niedziela Zwykła</span>
             </b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">7.30</td>
-            <td>+ z&nbsp;rodz. Witek i&nbsp;Podsiadły</td>
+            <td>+ Bogusława Stępińskiego 34&nbsp;rocz. śm.</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">9.30</td>
-            <td>+ Stanisława, Leokadię, Romana, Marię Majchrów,
-            Krzysztofa, Stefanię, Józefa Łukawskich, Jadwigę, Zofię,
-            Stanisława Karasińskich</td>
+            <td>+ Antoniego, Mieczysławę 4&nbsp;rocz.
+            śm. Koziełów, Annę Joniec 5&nbsp;rocz. śm.</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">11.15</td>
-            <td>K.Ż.R. – od&nbsp;Róży z&nbsp;Milczan</td>
+            <td>+ Otolię i&nbsp;Czesława Ciachów</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">12.30</td>
-            <td>Za&nbsp;parafian</td>
+            <td>O&nbsp;doświadczenie Bożej miłości, łaskę nawrócenia
+            dla męża Piotra i&nbsp;ojca Tadeusza</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>+ Mariana Klimonta, Aleksandrę Ciamagę i&nbsp;zm.
-            z&nbsp;rodz.</td>
+            <td>+ Stanisława Pacholczyka</td>
         </tr>
     </tbody>
 </table>
