89559fec00134e96e454f82ab3a6d9504222f793
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2024-11-16 14:14:15 +0000

    2024-11-16

--- archiwum13.php
+++ archiwum13.php
@@ -157,6 +157,62 @@ include 'head.php';
 
                         <div class="card">
                             <div class="card-header">
+                                <h3>Spotkania Duszpasterstwa Dzieci i&nbsp;Młodzieży<br>
+                                w&nbsp;parafii Podwyższenia Krzyża Świętego w&nbsp;Sandomierzu</h3>
+                            </div>
+                            <div class="col">
+                                <h4>
+                                    <strong>Zapraszamy na&nbsp;spotkania duszpasterskie dla
+                                    dzieci i&nbsp;młodzieży przygotowujących się do&nbsp;przyjęcia
+                                    Sakramentu Bierzmowania, Pierwszej Komunii świętej, oraz
+                                    przeżywających rocznicę Komunii świętej w&nbsp;naszej
+                                    parafii.</strong>
+                                </h4>
+                                <br>
+                                <ol>
+                                    <li>
+                                        <h5>KANDYDACI NA&nbsp;MINISTRANTA I&nbsp;MŁODSI MINISTRANCI</h5>
+                                        <p>Spotkanie w&nbsp;kościele 12&nbsp;października
+                                        o&nbsp;godz.&nbsp;11:30</p>
+                                    </li>
+
+                                    <li>
+                                        <h5>GRUPA MŁODZIEŻOWA</h5>
+                                        <p>Spotkanie w&nbsp;salce parafialnej 18&nbsp;października
+                                        o&nbsp;godz.&nbsp;18:30</p>
+                                    </li>
+
+                                    <li>
+                                        <h5>DZIECI PIERWSZOKOMUNIJNE I&nbsp;ROCZNICOWE</h5>
+                                        <p>Spotkanie w&nbsp;kościele 26&nbsp;października
+                                        o&nbsp;godz.&nbsp;10:00</p>
+                                    </li>
+
+                                    <li>
+                                        <h5>KANDYDACI DO&nbsp;BIERZMOWANIA (klasy 6, 7
+                                        i&nbsp;8)</h5>
+                                        <p>Spotkanie w&nbsp;kościele 27&nbsp;października
+                                        o&nbsp;godz.&nbsp;12:30</p>
+                                    </li>
+
+                                    <li>
+                                        <h5>ZAJĘCIA SPORTOWE NA&nbsp;HALI (dla szkoły
+                                        podstawowej)</h5>
+                                        <p>Hala sportowa przy ulicy Cieśli
+                                        w&nbsp;Sandomierzu</p>
+                                        <p>12&nbsp;października od&nbsp;12:15 do&nbsp;14:15</p>
+                                        <p>19&nbsp;października od&nbsp;14 do&nbsp;16</p>
+                                        <p>26&nbsp;października od&nbsp;14 do&nbsp;16</p>
+                                    </li>
+                                </ol>
+                                <h4 class="mt-2">Poprzednie wpisy przeniesiono do
+                                    <a href="archiwum.php">archiwum</a>.</h4>
+                            </div>
+                        </div>
+                        <br>
+
+                        <div class="card">
+                            <div class="card-header">
                                 <h3>INTENCJE MSZY ŚWIĘTYCH<br>
                                     4.11. – 10.11.2024&nbsp;r.
                                 </h3>
--- duszpasterstwo_dim.php
+++ duszpasterstwo_dim.php
@@ -19,26 +19,26 @@
         <ol>
             <li>
                 <h5>KANDYDACI NA&nbsp;MINISTRANTA I&nbsp;MŁODSI MINISTRANCI</h5>
-                <p>Spotkanie w&nbsp;kościele 12&nbsp;października
-                o&nbsp;godz.&nbsp;11:30</p>
+                <p>Spotkanie w&nbsp;kościele 30&nbsp;listopada
+                o&nbsp;godz.&nbsp;10:00</p>
             </li>
 
             <li>
                 <h5>GRUPA MŁODZIEŻOWA</h5>
-                <p>Spotkanie w&nbsp;salce parafialnej 18&nbsp;października
-                o&nbsp;godz.&nbsp;18:30</p>
+                <p>Spotkanie w&nbsp;salce parafialnej 23&nbsp;listopada
+                o&nbsp;godz.&nbsp;16:00, seans filmowy</p>
             </li>
 
             <li>
                 <h5>DZIECI PIERWSZOKOMUNIJNE I&nbsp;ROCZNICOWE</h5>
-                <p>Spotkanie w&nbsp;kościele 26&nbsp;października
-                o&nbsp;godz.&nbsp;10:00</p>
+                <p>Spotkanie dla rodziców w&nbsp;kościele
+                17&nbsp;listopada o&nbsp;godz.&nbsp;12:30</p>
             </li>
 
             <li>
                 <h5>KANDYDACI DO&nbsp;BIERZMOWANIA (klasy 6, 7
                 i&nbsp;8)</h5>
-                <p>Spotkanie w&nbsp;kościele 27&nbsp;października
+                <p>Spotkanie w&nbsp;kościele 1&nbsp;grudnia
                 o&nbsp;godz.&nbsp;12:30</p>
             </li>
 
@@ -47,9 +47,8 @@
                 podstawowej)</h5>
                 <p>Hala sportowa przy ulicy Cieśli
                 w&nbsp;Sandomierzu</p>
-                <p>12&nbsp;października od&nbsp;12:15 do&nbsp;14:15</p>
-                <p>19&nbsp;października od&nbsp;14 do&nbsp;16</p>
-                <p>26&nbsp;października od&nbsp;14 do&nbsp;16</p>
+                <p>23&nbsp;listopada od&nbsp;14 do&nbsp;16</p>
+                <p>30&nbsp;listopada od&nbsp;14 do&nbsp;16</p>
             </li>
         </ol>
         <h4 class="mt-2">Poprzednie wpisy przeniesiono do
